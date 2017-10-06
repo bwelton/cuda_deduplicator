3bbb51c31025b5332fffbf2a868a4afba849080a
Ben W <welton@cs.wisc.edu>
2017-10-06 18:41:37 -0500

    Added CUPTI fixes

--- src/deduplicate/CUPTIEventHandler.cpp
+++ src/deduplicate/CUPTIEventHandler.cpp
@@ -48,100 +48,12 @@ extern "C" {
 	const char * translateRuntimeCallback (CUpti_CallbackId id) {
 		if(RuntimeCIBMap.find(id) != RuntimeCIBMap.end())
 			return RuntimeCIBMap[id];
-		// switch(id) {
-		// 	case CUPTI_RUNTIME_TRACE_CBID_cudaFree_v3020:
-		// 		return "cudaFree";
-		// 	case CUPTI_RUNTIME_TRACE_CBID_cudaMalloc_v3020:
-		// 		return "cudaMalloc";
-		//     case CUPTI_RUNTIME_TRACE_CBID_cudaMemcpy_v3020:
-		//         return "cudaMemcpy";
-		//     case CUPTI_RUNTIME_TRACE_CBID_cudaMemcpy2D_v3020:
-		//         return "cudaMemcpy2D";
-		//     case CUPTI_RUNTIME_TRACE_CBID_cudaMemcpyToArray_v3020:
-		//         return "cudaMemcpyToArray";
-		//     case CUPTI_RUNTIME_TRACE_CBID_cudaMemcpy2DToArray_v3020:
-		//         return "cudaMemcpy2DToArray";
-		//     case CUPTI_RUNTIME_TRACE_CBID_cudaMemcpyFromArray_v3020:
-		//         return "cudaMemcpyFromArray";
-		//     case CUPTI_RUNTIME_TRACE_CBID_cudaMemcpy2DFromArray_v3020:
-		//         return "cudaMemcpy2DFromArray";
-		//     case CUPTI_RUNTIME_TRACE_CBID_cudaMemcpyArrayToArray_v3020:
-		//         return "cudaMemcpyArrayToArray";
-		//     case CUPTI_RUNTIME_TRACE_CBID_cudaMemcpy2DArrayToArray_v3020:
-		//         return "cudaMemcpy2DArrayToArray";
-		//     case CUPTI_RUNTIME_TRACE_CBID_cudaMemcpyToSymbol_v3020:
-		//         return "cudaMemcpyToSymbol";
-		//     case CUPTI_RUNTIME_TRACE_CBID_cudaMemcpyFromSymbol_v3020:
-		//         return "cudaMemcpyFromSymbol";
-		//     case CUPTI_RUNTIME_TRACE_CBID_cudaMemcpyAsync_v3020:
-		//         return "cudaMemcpyAsync";
-		//     case CUPTI_RUNTIME_TRACE_CBID_cudaMemcpyToArrayAsync_v3020:
-		//         return "cudaMemcpyToArrayAsync";
-		//     case CUPTI_RUNTIME_TRACE_CBID_cudaMemcpyFromArrayAsync_v3020:
-		//         return "cudaMemcpyFromArrayAsync";
-		//     case CUPTI_RUNTIME_TRACE_CBID_cudaMemcpy2DAsync_v3020:
-		//         return "cudaMemcpy2DAsync";
-		//     case CUPTI_RUNTIME_TRACE_CBID_cudaMemcpy2DToArrayAsync_v3020:
-		//         return "cudaMemcpy2DToArrayAsync";
-		//     case CUPTI_RUNTIME_TRACE_CBID_cudaMemcpy2DFromArrayAsync_v3020:
-		//         return "cudaMemcpy2DFromArrayAsync";
-		//     case CUPTI_RUNTIME_TRACE_CBID_cudaMemcpyToSymbolAsync_v3020:
-		//         return "cudaMemcpyToSymbolAsync";
-		//     case CUPTI_RUNTIME_TRACE_CBID_cudaMemcpyFromSymbolAsync_v3020:
-		//         return "cudaMemcpyFromSymbolAsync";
-		// 	default:
-		// 		break;
-		// }
 		return "<unknown>";
 	}
 
 	const char * translateDriverCallback (CUpti_CallbackId id) {
 		if (DriverCIBMap.find(id) != DriverCIBMap.end())
 			return DriverCIBMap[id];
-		// switch (id) {
-		//     case CUPTI_DRIVER_TRACE_CBID_cuMemcpyHtoD_v2:
-		//     	return "cuMemcpyHtoD_v2";
-		//     case CUPTI_DRIVER_TRACE_CBID_cuMemcpyHtoDAsync_v2:
-		//     	return "cuMemcpyHtoDAsync_v2";
-		//     case CUPTI_DRIVER_TRACE_CBID_cuMemcpyDtoH_v2:
-		//     	return "cuMemcpyDtoH_v2";
-		//     case CUPTI_DRIVER_TRACE_CBID_cuMemcpyDtoHAsync_v2:
-		//     	return "cuMemcpyDtoHAsync_v2";
-		//     case CUPTI_DRIVER_TRACE_CBID_cuMemcpyDtoD_v2:
-		//     	return "cuMemcpyDtoD_v2";
-		//     case CUPTI_DRIVER_TRACE_CBID_cuMemcpyDtoDAsync_v2:
-		//     	return "cuMemcpyDtoDAsync_v2";
-		//     case CUPTI_DRIVER_TRACE_CBID_cuMemcpyAtoH_v2:
-		//     	return "cuMemcpyAtoH_v2";
-		//     case CUPTI_DRIVER_TRACE_CBID_cuMemcpyAtoHAsync_v2:
-		//     	return "cuMemcpyAtoHAsync_v2";
-		//     case CUPTI_DRIVER_TRACE_CBID_cuMemcpyAtoD_v2:
-		//     	return "cuMemcpyAtoD_v2";
-		//     case CUPTI_DRIVER_TRACE_CBID_cuMemcpyDtoA_v2:
-		//     	return "cuMemcpyDtoA_v2";
-		//     case CUPTI_DRIVER_TRACE_CBID_cuMemcpyAtoA_v2:
-		//     	return "cuMemcpyAtoA_v2";
-		//     case CUPTI_DRIVER_TRACE_CBID_cuMemcpy2D_v2:
-		//     	return "cuMemcpy2D_v2";
-		//     case CUPTI_DRIVER_TRACE_CBID_cuMemcpy2DUnaligned_v2:
-		//     	return "cuMemcpy2DUnaligned_v2";
-		//     case CUPTI_DRIVER_TRACE_CBID_cuMemcpy2DAsync_v2:
-		//     	return "cuMemcpy2DAsync_v2";
-		//     case CUPTI_DRIVER_TRACE_CBID_cuMemcpy3D_v2:
-		//     	return "cuMemcpy3D_v2";
-		//     case CUPTI_DRIVER_TRACE_CBID_cuMemcpy3DAsync_v2:
-		//     	return "cuMemcpy3DAsync_v2";
-		//     case CUPTI_DRIVER_TRACE_CBID_cuMemcpyHtoA_v2:
-		//     	return "cuMemcpyHtoA_v2";
-		//     case CUPTI_DRIVER_TRACE_CBID_cuMemcpyHtoAAsync_v2:
-		//     	return "cuMemcpyHtoAAsync_v2";
-		//     case CUPTI_DRIVER_TRACE_CBID_cuMemcpy:
-		//     	return "cuMemcpy";
-		//     case CUPTI_DRIVER_TRACE_CBID_cuMemcpyAsync:
-		//     	return "cuMemcpyAsync";
-		//     default:
-		//     	break;
-		// }
 		return "<unknown>";
 	}
 
@@ -176,15 +88,6 @@ extern "C" {
 	 		std::string out = ss.str();	
 			_cupti_output->Write(out);	    	
 		}
-		// Doesn't exist in cuda 7.5......
-	  //   } else if (record->kind == CUPTI_ACTIVITY_KIND_SYNCHRONIZATION) {
-	  //   	CUpti_ActivitySynchronization *api = (CUpti_ActivitySynchronization *) record;
-	  //   	std::stringstream ss;
-	  //   	ss << "SYNC" << "," << api->contextId << "," << api->correlationId << "," << api->start - startTimestamp << "," << api->end - startTimestamp << "," << api->streamId << std::endl;    		
-	 	// 	std::string out = ss.str();	
-			// _cupti_output.get()->Write(out);	
-	  //   }
-
 	}
 	void bufCompleted(CUcontext ctx, uint32_t streamId, uint8_t *buffer, size_t size, size_t validSize) {
 	  	CUptiResult status;

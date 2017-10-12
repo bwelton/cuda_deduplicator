07f3b302ed92e2ff5a55811edb62ec2537a8fd7d
welton <welton@cs.wisc.edu>
2017-10-12 01:49:47 -0500

    Minor test changes

--- src/tests/cupti_test.cpp
+++ src/tests/cupti_test.cpp
@@ -4,31 +4,31 @@
 
 BOOST_AUTO_TEST_SUITE( basic_copy_cupti )
 
-void BasicCopyTest(void) {
-	RedirectOutputToFile();
-	std::shared_ptr<CudaCtx> tmp(new CudaCtx());
-	DeviceMemory<double> dev(2048*30);
-	dev.WriteAsync(tmp);
-	dev.WriteAsync(tmp);
-	dev.WriteAsync(tmp);
-	dev.ReadAsync(tmp);
-	dev.WriteSync();
-	dev.ReadAsync(tmp);
-	dev.WriteAsync(tmp);
-	dev.ReadAsync(tmp);
-	dev.WriteAsync(tmp);
-	dev.ReadSync();
-	// if (dev.CompareDevToCPU() == false) {
-	// 	exit(-1);
-	// }
-}
+// void BasicCopyTest(void) {
+// 	RedirectOutputToFile();
+// 	std::shared_ptr<CudaCtx> tmp(new CudaCtx());
+// 	DeviceMemory<double> dev(2048*30);
+// 	dev.WriteAsync(tmp);
+// 	dev.WriteAsync(tmp);
+// 	dev.WriteAsync(tmp);
+// 	dev.ReadAsync(tmp);
+// 	dev.WriteSync();
+// 	dev.ReadAsync(tmp);
+// 	dev.WriteAsync(tmp);
+// 	dev.ReadAsync(tmp);
+// 	dev.WriteAsync(tmp);
+// 	dev.ReadSync();
+// 	// if (dev.CompareDevToCPU() == false) {
+// 	// 	exit(-1);
+// 	// }
+// }
 
-BOOST_AUTO_TEST_CASE(basic_copy_test) {
-	// SANITY CHECK: Checks to make sure that we can copy something to/from the GPU.
-	// If this fails, something is wrong in the environment
-	TestingFixture t;
-	BOOST_REQUIRE(t.LaunchAndWait(boost::bind(BasicCopyTest)) == 0);
-}
+// BOOST_AUTO_TEST_CASE(basic_copy_test) {
+// 	// SANITY CHECK: Checks to make sure that we can copy something to/from the GPU.
+// 	// If this fails, something is wrong in the environment
+// 	TestingFixture t;
+// 	BOOST_REQUIRE(t.LaunchAndWait(boost::bind(BasicCopyTest)) == 0);
+// }
 
 void SynchTiming(void) {
 	RedirectOutputToFile();
@@ -62,7 +62,7 @@ BOOST_AUTO_TEST_CASE(synch_timing) {
 	// SANITY CHECK: Checks to make sure that we can copy something to/from the GPU.
 	// If this fails, something is wrong in the environment
 	TestingFixture t;
-	BOOST_REQUIRE(t.LaunchAndWait(boost::bind(BasicCopyTest)) == 0);
+	BOOST_REQUIRE(t.LaunchAndWait(boost::bind(SynchTiming)) == 0);
 }
 
 

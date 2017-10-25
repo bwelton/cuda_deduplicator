863cbcb92c4e3b76b0f3b8efb55277d021d9f94b
Benjamin Welton <welton@cs.wisc.edu>
2017-10-25 11:43:33 -0500

    Added

--- src/tests/PerfEstimateCheck.cpp
+++ src/tests/PerfEstimateCheck.cpp
@@ -49,6 +49,7 @@ std::pair<double, double> ProcessResults(void) {
 		// Child process, perform action then exit
 		// RedirectOutputToFile();
 		execlp("python", "CorrelationRun.py", "first_timeline.txt", "timing_info.txt", "timing_packet_corr.txt", "test_results.txt");
+		std::cerr << "COULD NOT LAUNCH PYTHON!" << std::cerr;
 		exit(-1);
 	} else {
 		pid_t tpid = 0;

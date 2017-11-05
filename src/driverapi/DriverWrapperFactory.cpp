4ebc3d884b24c150b35694fc5e53b5ab86e26519
Benjamin Welton <welton@cs.wisc.edu>
2017-11-05 17:47:09 -0600

    Added fixes

--- src/driverapi/DriverWrapperFactory.cpp
+++ src/driverapi/DriverWrapperFactory.cpp
@@ -1,8 +1,8 @@
 #include "DriverWrapperFactory.h"
+extern const std::vector<const char *> CallVector;
 DriverWrapperFactory::DriverWrapperFactory() { }
 DriverWrapperFactory::~DriverWrapperFactory() { }
 template <typename T> 
-extern static const std::vector<const char *> CallVector;
 int DriverWrapperFactory::PerformAction(DriverAPICall t, T params) {
 	int call = std::get<0>(params);
 	std::cerr << "Call to " << CallVector[call] << " was made" << std::endl;

eff0a8bff72aa6aa31bd7e96b2ccf5575e707003
Benjamin Welton <welton@cs.wisc.edu>
2017-11-08 14:49:18 -0600

    Added

--- src/driverapi/dlsym_wrapper.cpp
+++ src/driverapi/dlsym_wrapper.cpp
@@ -1,24 +1,25 @@
-#include <dlfcn.h>
 #include <string>
+#define __USE_GNU
+#include <dlfcn.h>
 #include <stdio.h>
 extern "C" {
-void * __real_dlopen(const char *filename, int flags);
-void *__wrap_dlopen (const char *filename, int flags) {
+void * __libc_dlopen(const char *filename, int flags);
+void *dlopen(const char *filename, int flags) {
 	if (filename != NULL) {
 		void * ret;
 		std::string tmp = std::string((char *)filename);
-		ret =  __real_dlopen(filename, flags);
+		ret =  __libc_dlopen(filename, flags);
 		fprintf(stderr, "DLOPEN: %s HANDLE: %p\n", filename, ret);
 		return ret;
 	} else {
-		return __real_dlopen(filename, flags);
+		return __libc_dlopen(filename, flags);
 	}
 }
-void * __real_dlsym(void * handle, const char * symbol);
-void * __wrap_dlsym(void * handle, const char * symbol) {
+void * __libc_dlsym(void * handle, const char * symbol);
+void * dlsym(void * handle, const char * symbol) {
 	if (symbol != NULL) {
 		fprintf(stderr, "DLSYMBOL: %s, HANDLE: %p\n", symbol, handle);
 	}
-	return __real_dlsym(handle, symbol);
+	return __libc_dlsym(handle, symbol);
 }
 }

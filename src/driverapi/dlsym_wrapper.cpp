1073a023f28a5dc2eacc09c241dab916e066663c
Benjamin Welton <welton@cs.wisc.edu>
2017-11-08 14:06:39 -0600

    Added

--- src/driverapi/dlsym_wrapper.cpp
+++ src/driverapi/dlsym_wrapper.cpp
@@ -1,6 +1,8 @@
 #include <dlfcn.h>
+#include <string>
 #include <stdio.h>
 extern "C" {
+void * __real_dlopen(const char *filename, int flags);
 void *__wrap_dlopen (const char *filename, int flags) {
 	if (filename != NULL) {
 		void * ret;
@@ -12,7 +14,7 @@ void *__wrap_dlopen (const char *filename, int flags) {
 		return __real_dlopen(filename, flags);
 	}
 }
-
+void * __real_dlsym(void * handle, const char * symbol);
 void * __wrap_dlsym(void * handle, const char * symbol) {
 	if (symbol != NULL) {
 		fprintf(stderr, "DLSYMBOL: %s, HANDLE: %p\n", symbol, handle);

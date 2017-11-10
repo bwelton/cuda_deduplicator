6452dcabd4dc84b2656890f7dda83974e81ee453
Benjamin Welton <welton@cs.wisc.edu>
2017-11-10 16:42:00 -0600

    Added

--- src/tests/basic_checker_test.cpp
+++ src/tests/basic_checker_test.cpp
@@ -68,6 +68,7 @@ BOOST_AUTO_TEST_CASE(check_write_size_sync) {
 	std::cerr << "Expected first value: " << 20 << " expected second value: " << expected << std::endl;
 	BOOST_CHECK(ret.first == 20);
 	BOOST_CHECK(ret.second == expected);
+	exit(-1);
 }
 
 void CheckWriteASyncSize(std::vector<int> sizes) {

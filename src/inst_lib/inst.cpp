ae948f92222217f36307686488c80e55585c13df
Benjamin Welton <welton@cs.wisc.edu>
2017-11-29 12:22:11 -0600

    Added

--- src/inst_lib/inst.cpp
+++ src/inst_lib/inst.cpp
@@ -197,7 +197,7 @@ void InsertSymbols(InstStorage * storage, char * outputName) {
 	free(modname);
 
 	assert(cudaMod != NULL);
-	Dyninst::SymtabAPI *symtab =  Dyninst::SymtabAPI::convert(cudaMod->getObject());
+	Symtab *symtab =  Dyninst::SymtabAPI::convert(cudaMod->getObject());
 
 	for(auto i : storage->SymbolsToWrite){
 		if(symtab->createFunction(i.second, i.first, 0) == NULL){

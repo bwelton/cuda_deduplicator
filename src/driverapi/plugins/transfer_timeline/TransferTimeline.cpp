e8805b7913cab4ca1fea753a435e2a4c3a30a12b
Benjamin Welton <welton@cs.wisc.edu>
2017-12-18 14:53:02 -0600

    Minor rename

---
+++ src/driverapi/plugins/transfer_timeline/TransferTimeline.cpp
@@ -0,0 +1,46 @@
+#include "TransferTimeline.h"
+
+PluginReturn TransferTimeline::Precall(std::shared_ptr<Parameters> params) {
+	if (params.get()->MemoryTransfer->IsSupportedTransfer() == false)
+		return NO_ACTION;
+
+	std::cerr << "[TRANSFER-TIMELINE] - Captured transfer: " << params.get()->GetName() << std::endl;
+	return NO_ACTION;
+}
+
+PluginReturn TransferTimeline::Postcall(std::shared_ptr<Parameters> params) {
+	if (params.get()->MemoryTransfer->IsSupportedTransfer() == false)
+		return NO_ACTION;
+
+	std::cerr << "[TRANSFER-TIMELINE] - Captured transfer Completed: " << params.get()->GetName() << std::endl;
+	return NO_ACTION;
+}
+
+TransferTimeline::TransferTimeline(std::vector<std::string> & cmd_list) {
+	exited = 0;
+}
+
+TransferTimeline::~TransferTimeline() {
+	exited = 1;
+}
+
+
+extern "C"{
+
+void init(std::vector<std::string> & cmd_list) {
+	PLUG_BUILD_FACTORY(cmd_list)
+}
+
+PluginReturn Precall(std::shared_ptr<Parameters> params){
+	if (exited == 1)
+		return NO_ACTION;
+	return PLUG_FACTORY_PTR->Precall(params);
+}
+
+PluginReturn Postcall(std::shared_ptr<Parameters> params) {
+	if (exited == 1)
+		return NO_ACTION;
+	return PLUG_FACTORY_PTR->Postcall(params);
+}
+
+}

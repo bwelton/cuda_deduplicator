41fe762a52b6994406912cad9bd3037a7e10708b
Benjamin Welton <welton@cs.wisc.edu>
2018-01-01 21:19:44 -0600

    Added

---
+++ src/driverapi/launcher/TimeApplications.cpp
@@ -0,0 +1,19 @@
+#include "TimeApplications.h"
+
+TimeApplications::TimeApplications(boost::program_options::variables_map vm) :
+	_vm(vm) {
+
+}
+
+uint64_t TimeApplications::Run() {
+	ProcessController proc(_vm);
+	proc.LaunchProcess();
+	auto start = std::chrono::high_resolution_clock::now();
+	while (!proc.IsTeminated())
+		proc.ContinueExecution();
+	auto stop = std::chrono::high_resolution_clock::now();
+	std::chrono::duration<uint64_t> diff = end-start;
+	std::cerr << "[TIMEAPP] Application runtime without instrimentation - " << diff.count() << std::endl;
+	return diff.count();	
+}
+
---
+++ src/driverapi/launcher/TimeApplications.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <string.h>
+#include <algorithm>
+#include <functional>
+#include <array>
+#include <iostream>
+#include <cassert>
+#include <deque>
+#include <sys/time.h>
+#include <cstdlib>
+#include <sstream>
+#include <tuple>
+#include <utility> 
+#include <stdarg.h>
+#include <map>
+#include <set> 
+#include <iomanip>
+#include <string>
+#include <sys/types.h>
+#include <unistd.h>
+#include <mutex>
+#include <boost/program_options.hpp>
+#include <numeric>
+#include <chrono>
+#include "ProcessControl.h"
+
+class TimeApplications {
+private:
+	TimeApplications(boost::program_options::variables_map vm);
+	uint64_t Run();
+public:
+	boost::program_options::variables_map _vm;
+};

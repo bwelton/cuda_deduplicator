00e84a42e14ac31ef49ed8d0ccec8485564696f9
Benjamin Welton <welton@cs.wisc.edu>
2018-01-08 18:28:14 -0600

    Added

--- src/driverapi/launcher/ProcessControl.cpp
+++ src/driverapi/launcher/ProcessControl.cpp
@@ -248,7 +248,7 @@ void ProcessController::InstrimentApplication() {
 					Dyninst::SymtabAPI::Region * reg = sym->getRegion();
 					std::vector<Dyninst::SymtabAPI::relocationEntry> entries = reg->getRelocations();
 					for (auto mn : entries)
-						if(mn.name.find("ORIGINAL_cuInit") != std::string::npos)
+						if(mn.name().find("ORIGINAL_cuInit") != std::string::npos)
 							std::cerr << "[PROCCTR] Found Relocation Entry - " << std::hex << mn.target_addr() << std::dec 
 						              << "," << std::hex << mn.rel_addr() << std::dec << std::endl;
 					std::cerr << "[PROCCTR] Function " << orig[0]->getName() << " wrapped successful" << std::endl;

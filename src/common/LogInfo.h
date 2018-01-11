032d5b48f0344c9bfff56e436b0533ccfb854a33
Benjamin Welton <welton@cs.wisc.edu>
2018-01-10 19:00:25 -0600

    Begin testing changes

--- src/common/LogInfo.h
+++ src/common/LogInfo.h
@@ -10,8 +10,10 @@ private:
 	mutable boost::recursive_mutex _log_mtx;
 	FILE * _fd;	
 	std::string _header;
+	bool _enabled;
 public:
-	LogInfo(FILE * fd = stderr) {
+	LogInfo(FILE * fd = stderr, bool enabled = true) {
+		_enabled = enabled;
 		_header = std::string("");
 		if (fd < 0){
 			_fd = stderr;
@@ -22,30 +24,41 @@ public:
 		}
 	}
 
-	LogInfo(std::string filename, std::string header) {
-		_fd = fopen(filename.c_str(), "w");
-		if (_fd == NULL)  {
-			fprintf(stderr, "%s %s\n", "[LOGINFO] Could not open output file, defaulting to stdout: ", filename.c_str());
-			_fd = stdout;
+	LogInfo(std::string filename, std::string header, bool enabled) {
+		_enabled = enabled;
+		if (enabled) {
+			_fd = fopen(filename.c_str(), "w");
+			if (_fd == NULL)  {
+				fprintf(stderr, "%s %s\n", "[LOGINFO] Could not open output file, defaulting to stdout: ", filename.c_str());
+				_fd = stdout;
+			}
+			_header = std::string("[") + header + std::string("]");
 		}
-		_header = std::string("[") + header + std::string("]");
 	}
 
 	~LogInfo() {
+		if (!_enabled)
+			return;
 		fflush(_fd);
 		fclose(_fd);
 	}
 
 	void Flush() {
+		if (!_enabled)
+			return;
 		fflush(_fd);
 	}
 	void Write(std::string & out) {
+		if (!_enabled)
+			return;
 		{
 			boost::recursive_mutex::scoped_lock lock(_log_mtx);
 			fprintf(_fd, "%s %s", _header.c_str(), out.c_str());
 		}
 	}
 	void Write(char * fmt, ...) {
+		if (!_enabled)
+			return;
 		{
 			boost::recursive_mutex::scoped_lock lock(_log_mtx);
 		    va_list ap;
@@ -55,6 +68,8 @@ public:
 		}
 	}
 	void Write(char * fmt, va_list args) {
+		if (!_enabled)
+			return;
 		{
 			boost::recursive_mutex::scoped_lock lock(_log_mtx);
 		    vfprintf(_fd, fmt, args);

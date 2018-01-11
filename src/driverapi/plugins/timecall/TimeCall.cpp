811abdccc9393cdd50bcab8495990d478966c7b7
Benjamin Welton <welton@cs.wisc.edu>
2018-01-11 16:06:14 -0600

    Added

---
+++ src/driverapi/plugins/timecall/TimeCall.cpp
@@ -0,0 +1,61 @@
+#include "TimeCall.h"
+std::shared_ptr<TimeCall> Worker;
+
+TimeCall::TimeCall(std::vector<std::string> & cmd_list) {
+	std::ifstream infile("FuncsToTime.txt");
+	std::string line;
+	while (std::getline(infile, line)) {
+		for (int i = 0; i < cmd_list.size(); i++) {
+			if (cmd_list[i] == line){
+				_callsToMonitor.insert(i);
+				break;
+			}
+		}
+	}
+}
+
+TimeCall::~TimeCall() {
+	if(_timers.size() > 0) {
+		FILE * out = fopen("call_timers.txt","w");
+		for(auto i : _timers)
+			fprintf(out, "%llu,%f\n",i.first, i.second);
+		fclose(out);
+		_timers.clear();
+	}
+}
+
+PluginReturn TimeCall::Precall(std::shared_ptr<Parameters> params) {
+	if (_callsToMonitor.find(params.get()->GetID()) != _callsToMonitor.end()) {
+		auto start = std::chrono::high_resolution_clock::now();
+		params.get()->Call();
+		auto stop = std::chrono::high_resolution_clock::now();
+		std::chrono::duration<double> diff = stop-start;
+		_timers.push_back(std::make_pair(params.get()->GetInstID(), diff.count()));
+		return PERFORMED_ACTION;
+	}
+	return NO_ACTION;
+}
+
+PluginReturn TimeCall::Postcall(std::shared_ptr<Parameters> params) {
+	return NO_ACTION;
+}
+
+extern "C"{
+
+void init(std::vector<std::string> & cmd_list) {
+	PLUG_BUILD_FACTORY(cmd_list)
+}
+
+PluginReturn Precall(std::shared_ptr<Parameters> params){
+	if (exited == 1)
+		return NO_ACTION;
+	return PLUG_FACTORY_PTR->Precall(params);
+}
+
+PluginReturn Postcall(std::shared_ptr<Parameters> params) {
+	if (exited == 1)
+		return NO_ACTION;
+	return PLUG_FACTORY_PTR->Postcall(params);
+}
+
+}

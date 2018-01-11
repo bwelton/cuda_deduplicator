acece60dfbd3adb0bdad94c3251865fa82f26691
Benjamin Welton <welton@cs.wisc.edu>
2018-01-10 18:52:43 -0600

    Begin testing changes

--- src/common/LogInfo.h
+++ src/common/LogInfo.h
@@ -2,14 +2,17 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <boost/thread/recursive_mutex.hpp>
+#include <string>
 
 // Lock to ensure single entry output
 class LogInfo {
 private:
 	mutable boost::recursive_mutex _log_mtx;
 	FILE * _fd;	
+	std::string _header;
 public:
 	LogInfo(FILE * fd = stderr) {
+		_header = std::string("");
 		if (fd < 0){
 			_fd = stderr;
 			fprintf(stderr, "%s\n", "[LOGINFO] Could not open output file, using stderr");
@@ -17,6 +20,15 @@ public:
 		else {
 			_fd = fd; 
 		}
+	}
+
+	LogInfo(std::string filename, std::string header) {
+		_fd = fopen(filename.c_str(), "w");
+		if (_fd == NULL)  {
+			fprintf(stderr, "%s %s\n", "[LOGINFO] Could not open output file, defaulting to stdout: ", filename.c_str());
+			_fd = stdout;
+		}
+		_header = std::string("[") + header + std::string("]");
 	}
 
 	~LogInfo() {
@@ -30,7 +42,7 @@ public:
 	void Write(std::string & out) {
 		{
 			boost::recursive_mutex::scoped_lock lock(_log_mtx);
-			fprintf(_fd, "%s", out.c_str());
+			fprintf(_fd, "%s %s", _header.c_str(), out.c_str());
 		}
 	}
 	void Write(char * fmt, ...) {

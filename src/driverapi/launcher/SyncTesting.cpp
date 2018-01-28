51381db2a9e24ff3bfe398458fb1aa2ee86e9691
Benjamin Welton <welton@cs.wisc.edu>
2018-01-27 22:54:32 -0600

    Added

--- src/driverapi/launcher/SyncTesting.cpp
+++ src/driverapi/launcher/SyncTesting.cpp
@@ -36,6 +36,7 @@ void SyncTesting::GatherSynchronizationCalls() {
 	// Discard Time, we dont care about it here, we only want the names of the funcitons that
 	// synchronize. 
 	std::vector<std::tuple<std::string, std::string, std::string, std::string, std::string> > extras;
+	extras.push_back(std::make_tuple(std::string("wrap"), std::string(INTERNAL_SYNC), std::string("INTER_InternalSynchronization"), std::string(DRIVER_LIBRARY), std::string("ORIGINAL_InternalSynchronization")));
 	double time = base.RunWithInstrimentation(def, extras);
 }
 

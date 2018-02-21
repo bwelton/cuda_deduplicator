9d92af6644c6e8cd6d1ef0f1bc8e9b89340a84e7
Benjamin Welton <welton@cs.wisc.edu>
2018-02-21 17:02:31 -0600

    Added

--- src/driverapi/launcher/LoadStoreInst.cpp
+++ src/driverapi/launcher/LoadStoreInst.cpp
@@ -52,7 +52,7 @@ bool LoadStoreInst::InstrimentAllModules(bool finalize) {
 	axs.insert(BPatch_opStore);
 
 	while (funcsToInstriment.empty() == false) {
-		if (_funcId > 280)
+		if (_funcId > 278)
 			break;
 		BPatch_function * x = funcsToInstriment.front();
 		funcsToInstriment.pop();

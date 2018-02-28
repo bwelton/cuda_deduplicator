4858820db33a7aceb0f85c3e6ffb973bb0d88f01
Benjamin Welton <welton@cs.wisc.edu>
2018-02-28 13:09:06 -0600

    Added

--- src/driverapi/launcher/SymbolLookup.cpp
+++ src/driverapi/launcher/SymbolLookup.cpp
@@ -23,20 +23,31 @@ bool SymbolLookup::GetInfoAtLocation(uint64_t offset, std::pair<std::string, Lin
 		return false;
 	}
 	std::vector<Symbol *> ret = _obj->findSymbolByOffset(offset);
+	SymtabAPI::Function * func = NULL;
+	_obj->getContainingFunction(offset, func);
+	if (func == NULL) {
+#ifdef DEBUG_SYMBOLLOOKUP
+		std::cerr << "Did not return a function for this point" << std::endl;
+#endif		
+		return false;
+	}
+	lines.first = func->getPrettyName();
+	lines.second.filename = func->getModule()->fullName();
+	lines.second.lineNum = 0;
 	if (ret.size() == 0) {
 #ifdef DEBUG_SYMBOLLOOKUP
 		std::cerr << "Did not return a symbol" << std::endl;
 #endif
 		return false;
 	}
+
 	if (ret[0]->isFunction() == false){
 		return false;
 	}
-	lines.first = ret[0]->getPrettyName();
 	std::vector<Dyninst::SymtabAPI::Statement::Ptr> lineNumbers;
 	_obj->getSourceLines(lineNumbers, offset);
 	if (lineNumbers.size() > 0) {
-		lines.second.filename = lineNumbers[0]->getFile();
+		//lines.second.filename = lineNumbers[0]->getFile();
 		lines.second.lineNum = lineNumbers[0]->getLine();
 	}
 	return true;

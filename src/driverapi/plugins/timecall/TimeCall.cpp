30d170da3a14caa050810e54700df41c8ac34dac
Benjamin Welton <welton@cs.wisc.edu>
2018-03-01 17:00:58 -0600

    Added

--- src/driverapi/plugins/timecall/TimeCall.cpp
+++ src/driverapi/plugins/timecall/TimeCall.cpp
@@ -68,7 +68,7 @@ void TIMER_SIMPLE_TIME_START(const char * callName) {
 		_timingLog.reset(new LogInfo(fopen("callDelay.out", "w")));
 	}
 	if (TimingCount.size() > 0)
-		std::cerr << "We are already tracking a previous synchronization" << std::cerr;
+		std::cerr << "We are already tracking a previous synchronization" << std::endl;
 	TimingCount.push_back(0);
 	TimingPairs.push_back(std::make_pair(std::string(callName),std::chrono::high_resolution_clock::now()));
 }

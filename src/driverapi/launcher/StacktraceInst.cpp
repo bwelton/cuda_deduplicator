b7f54d1d9905e260ecfc3d96ce3db171266ea0de
Benjamin Welton <welton@cs.wisc.edu>
2018-03-13 17:23:07 -0500

    More changes

---
+++ src/driverapi/launcher/StacktraceInst.cpp
@@ -0,0 +1,135 @@
+#include "StacktraceInst.h"
+
+StacktraceInst::StacktraceInst(BPatch_addressSpace * addrSpace, BPatch_image * img) :
+	_addrSpace(addrSpace), _img(img), _curID(0) {
+	
+
+}
+
+
+void StacktraceInst::InsertStackInst() {
+	Setup();
+	// Get a list of all functions in the binary
+	std::vector<BPatch_function *> all_functions;
+	_img->getProcedures(all_functions);
+
+	// Begin the insertion set
+	_addrSpace->beginInsertionSet();
+
+	// The strategy here is to insert an entry call at all functions calls in the function to denote entrance
+	// into the underlying function and the position of that entrance (i.e. the specific call it entered from).
+	// We will also record the exit from this function. 
+	for (auto i : all_functions) {
+		if (IsFunctionExcluded(i))
+			continue;
+		uint64_t myID =  GetFuncId(i);	
+		// Get points
+		std::vector<BPatch_function * func, BPatch_point *> calls = GetFunctionCalls(i);
+		for (auto n : calls) {
+			InsertEntryExitCall(i, n.first, n.second);
+		}
+	}
+
+	// Insert call to capture synchronization specifically
+	BPatch_function * cudaSync = NULL;
+	Dyninst::Address offsetAddress = 0;
+	std::vector<BPatch_object *> imgObjs;
+	_img->getObjects(imgObjs);
+	for (auto i : imgObjs){
+		if (i->name().find("libcuda.so") == std::string::npos)
+			continue;
+		// Found libcuda
+		offsetAddress = i->fileOffsetToAddr(INTERNAL_SYNC_ST);
+		cudaSync = _img->findFunction(offsetAddress);
+		break;
+	}
+	assert(cudaSync != NULL);
+	{
+		std::vector<BPatch_point*> * funcEntry = cudaSync->findPoint(BPatch_locEntry);
+		std::vector<BPatch_snippet*> testArgs;
+		BPatch_funcCallExpr recordFuncEntry(*_recordSync, testArgs);
+		std::cerr << "Adding Sync Notifyer" << std::endl;
+		if (_addrSpace->insertSnippet(recordFuncEntry,*funcEntry) == NULL) 
+			std::cerr << "could not insert func entry snippet" << std::endl;
+	}
+
+	_addrSpace->finalizeInsertionSet(false);	
+}
+
+void StacktraceInst::InsertEntryExitCall(BPatch_function * instFunc, BPatch_function * calledFunc, BPatch_point * callPoint) {
+	if (IsFunctionExcluded(calledFunc))
+		return;
+	
+	uint64_t id = GetFuncId(calledFunc);
+
+	// Before the point, insert an entry call.
+	// After the point, insert an exit call.
+	
+	std::vector<BPatch_snippet*> testArgs;
+	testArgs.push_back(new BPatch_constExpr(id));
+	testArgs.push_back(new BPatch_constExpr(((uint64_t)callPoint->getAddress()) - (uint64_t)instFunc->getModule()->getBaseAddr()));
+	BPatch_funcCallExpr recordFuncEntry(*_entryFunc, testArgs);
+	//std::cerr << x->getName() << "," << _funcId << std::endl;
+	if (_addrSpace->insertSnippet(recordFuncEntry,*callPoint,BPatch_callBefore) == NULL)
+		std::cerr << "Could not insert entry snippet - " << calledFunc->getName() << std::endl;
+
+	BPatch_funcCallExpr recordFuncExit(*_exitFunc, testArgs);	
+	if (_addrSpace->insertSnippet(recordFuncExit,*callPoint,BPatch_callAfter) == NULL)
+		std::cerr << "Could not insert exit snippet - " << calledFunc->getName() << std::endl;
+}
+
+std::vector<BPatch_function * func, BPatch_point *> StacktraceInst::GetFunctionCalls(BPatch_function * func) {
+	std::vector<BPatch_point*> * callPoints = func->findPoint(BPatch_locSubroutine);
+	std::vector<BPatch_function * func, BPatch_point *> ret;
+	for (auto i : *callPoints) {
+		ret.push_back(std::make_pair(i->getCalledFunction(), i));
+	}
+	return ret;
+}
+
+uint64_t StacktraceInst::GetFuncId(BPatch_function * func) {
+	uint64_t baseAddr = (uint64_t)func->getBaseAddr();
+	if (_idMap.find(baseAddr) != _idMap.end()) {
+		return _idMap[baseAddr].first;
+	}
+	// doesn't exist, add it.
+	_idMap[baseAddr] = std::make_pair(_curID, func);
+	_curID += 1;
+	return _curID - 1;
+}
+
+void StacktraceInst::Setup() {
+	std::vector<BPatch_function *> entryFunc;
+	std::vector<BPatch_function *> exitFunc;
+	std::vector<BPatch_function *> recordSync;
+
+	_img->findFunction("STACKTRACE_RECORD_ENTRY", entryFunc);
+	_img->findFunction("STACKTRACE_RECORD_EXIT", exitFunc);	
+	_img->findFunction("SYNC_RECORD_SYNC_CALL", recordSync);	
+
+	assert(entryFunc.size() > 0);
+	assert(exitFunc.size() > 0);
+	assert(recordSync.size() > 0);
+
+
+	_entryFunc = entryFunc[0];
+	_exitFunc = exitFunc[0];
+	_recordSync = recordSync[0];
+}
+
+
+bool StacktraceInst::IsFunctionExcluded(BPatch_function * func) {
+	static StringVector skippedNames = {"cuda_deduplicator", "dyninst", "boost","libCUPTIEventHandler", "cudadedup", "libdl-2.23.so","libpthread-2.23.so","libc.so.6","libm.so.6", "exit"};
+	std::string funcName = func->getName();
+	std::string objectName = func->getModule()->getObject()->pathName();
+	for (auto i :skippedNames){
+		if (funcName.find(i) != std::string::npos)
+			return true;
+		if (objectName.find(i) != std::string::npos)
+			return true;
+	}
+	// If this is the sync call, skip it.
+	if ((uint64_t)func->getBaseAddr() == INTERNAL_SYNC_ST)
+		return true;
+	return false;
+}
---
+++ src/driverapi/launcher/StacktraceInst.h
@@ -0,0 +1,75 @@
+#pragma once
+#include <string.h>
+#include <algorithm>
+#include <functional>
+#include <array>
+#include <iostream>
+#include <cassert>
+#include <deque>
+#include <sys/time.h>
+#include <cstdlib>
+#include <sstream>
+#include <tuple>
+#include <utility> 
+#include <stdarg.h>
+#include <map>
+#include <set> 
+#include <iomanip>
+#include <string>
+#include <sys/types.h>
+#include <unistd.h>
+#include <mutex>
+#include <queue>
+#include <boost/program_options.hpp>
+
+// Dyninst includes
+#include "CodeObject.h"
+#include "CFG.h"
+#include "PatchObject.h"
+#include "PatchMgr.h"
+#include "Point.h"
+#include "BPatch_object.h"
+#include "BPatch_snippet.h"
+#include "BPatch.h"
+#include "BPatch_binaryEdit.h"
+#include "BPatch_image.h"
+#include "BPatch_function.h"
+#include "BPatch_Vector.h"
+#include "BPatch_point.h"
+#include "BPatch_addressSpace.h"
+#include "BPatch_statement.h"
+#include "dynC.h"
+#include "set"
+#include "LogInfo.h"
+#include "Constants.h"
+#include "StackPoint.h"
+using namespace Dyninst;
+using namespace ParseAPI;
+using namespace PatchAPI;
+using namespace SymtabAPI;
+
+#define INTERNAL_SYNC_ST 0x2864C0
+typedef std::pair<uint64_t, BPatch_function *> FuncIDPair;
+class StacktraceInst {
+public:
+	StacktraceInst(BPatch_addressSpace * addrSpace, BPatch_image * img);
+	void Setup();
+	bool IsFunctionExcluded(BPatch_function * func);
+	void InsertStackInst();
+	uint64_t GetFuncId(BPatch_function * func);
+	void InsertEntryExitCall(BPatch_function * instFunc, BPatch_function * calledFunc, BPatch_point * callPoint);
+	std::vector<BPatch_function * func, BPatch_point *> GetFunctionCalls(BPatch_function * func);
+
+private:
+	BPatch_function * _entryFunc;
+	BPatch_function * _exitFunc;
+	BPatch_function * _recordSync;
+
+	BPatch_addressSpace * _addrSpace;
+	BPatch_image * _img;
+
+	// Stores the current id
+	uint64_t _curID;
+	// Offset to <bpatch_function, id> map
+	std::map<uint64_t, FuncIDPair > _idMap; 
+};

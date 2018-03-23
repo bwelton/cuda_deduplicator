e2d8b80d6b613ed8807ca5c36f215a52ce2c4db8
Benjamin Welton <welton@cs.wisc.edu>
2018-03-23 13:11:06 -0500

    Added

--- src/tests/SmallModify.cpp
+++ src/tests/SmallModify.cpp
@@ -59,6 +59,7 @@ int main(const int argc, const char * argv[]){
 	patch.setInstrStackFrames(false);
 	BPatch_binaryEdit * app = patch.openBinary(argv[1], true);
 	patch.setInstrStackFrames(false);
+	patch.setSaveFPR(false);
 	BPatch_image * img = app->getImage();
 	app->loadLibrary("/nobackup/spack_repo/opt/spack/linux-ubuntu16.04-x86_64/gcc-6.4.0/cudadedup-develop-mbsbiqg2zylptsgokmkjiehitydyfwtq/lib/plugins/libStacktrace.so");
 	//BPatch_object * obj = app->loadLibrary("libcuda.so.1");

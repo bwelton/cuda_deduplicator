6b87392acda34746bed5a0fa48a511dc5a112c64
Benjamin Welton <welton@cs.wisc.edu>
2018-03-27 16:12:48 -0500

    Added

--- src/driverapi/launcher/PerformanceModel.cpp
+++ src/driverapi/launcher/PerformanceModel.cpp
@@ -234,7 +234,7 @@ void PerformanceModel::ExtractLineInfo() {
 	// Decode line info for every stack
 	for (auto i : _stackPoints) {
 		_lineInfo[i.first] = std::vector<std::pair<std::string, LineInfo> >();
-		for(auto z : i.second) {
+		for(auto & z : i.second) {
 			// Convert to line info and add  to _lineInfo
 			if (symbolInfo.find(z.libname) == symbolInfo.end())
 				continue;

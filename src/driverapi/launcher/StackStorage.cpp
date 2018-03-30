cbcca905d83f46b4c7704207ee644d2758577272
Benjamin Welton <welton@cs.wisc.edu>
2018-03-29 22:16:42 -0500

    Added

---
+++ src/driverapi/launcher/StackStorage.cpp
@@ -0,0 +1,46 @@
+#include "StackStorage.h"
+
+StackRecord::StackRecord(uint64_t id, std::vector<StackPoint> & points) : _id(id), _points(points) {
+
+}
+
+StackPoint StackRecord::GetFirstCudaCall() {
+	for (int i = _points.size() - 1; i >= 0; i = i - 1){
+		if(_points[i].libname.find("libcuda.so") != std::string::npos){
+#ifdef DEBUG_STACKRECORD
+			std::cout << "[StackRecord] First entry into libcuda for Stack Record " << _id << " is " << _points[i].funcName << std::endl;
+#endif
+			return _points[i];
+		}
+	}	
+	StackPoint empty;
+	empty.empty = true;
+	return empty;
+}
+
+void StackRecord::AddStackRecord(uint64_t pos) {
+	if (_ranges.size() == 0){
+		_ranges.push_back(SyncRangeRecord(pos));
+		return;
+	}
+	if (_ranges.back().ExtendRange(pos) == false) {
+		_ranges.push_back(SyncRangeRecord(pos));
+	}
+}
+
+void StackRecord::GetStackSymbols(SymbolMap & m) {
+	for (int i = 0; i < _points.size(); i++) {
+		if (m.find(_points[i].libname) == m.end())
+			continue;
+		std::pair<std::string, LineInfo> tmp;
+		m[_points[i].libname]->GetInfoAtLocation(_points[i].libOffset, tmp);
+		_points[i].funcOffset = m[_points[i].libname]->GetFunctionOffset(_points[i].libOffset);
+		_points[i].funcName = m[_points[i].libname]->GetFuncName(_points[i].libOffset);
+		_points[i].empty = false;
+	}
+#ifdef DEBUG_STACKRECORD
+	std::cout << "[StackRecord] Decoded Stack for ID - " << _id << std::endl;
+	for (auto i : _points)
+		std::cout << "[StackRecord]\t" << i.funcName << "@" << i.libOffset << " in " << i.libname << std::endl;
+#endif 
+}
---
+++ src/driverapi/launcher/StackStorage.h
@@ -0,0 +1,44 @@
+#include "StackPoint.h"
+#include "SymbolLookup.h"
+
+#define DEBUG_STACKRECORD 1
+
+typedef std::map<std::string, std::shared_ptr<SymbolLookup> > SymbolMap;
+
+struct SyncRangeRecord {
+	uint64_t start;
+	uint64_t end;
+	SyncRangeRecord(uint64_t i) {
+		start = i;
+		end = i;
+	};
+
+	bool ExtendRange(uint64_t i) {
+		if (end + 1 == i){
+			end = end + 1;
+			return true;
+		}
+		return false;
+	};
+	bool InRange(uint64_t i) {
+		if (i == start || i == end || (i > start && i < end))
+			return true;
+		return false;
+	};
+};
+
+
+class StackRecord {
+public:
+	StackRecord(uint64_t id, std::vector<StackPoint> & points);
+	void GetStackSymbols(SymbolMap & m);
+	void AddStackRecord(uint64_t pos);
+	StackPoint GetFirstCudaCall();
+private:
+	uint64_t _id;
+	std::vector<StackPoint> _points;
+	std::vector<SyncRangeRecord> _ranges;
+};
+
+
+

452d6ed26d4bcfbb6e9971d5d7dcad7321a0c65b
Benjamin Welton <welton@cs.wisc.edu>
2018-04-06 15:46:25 -0500

    Added

--- src/driverapi/plugins/stacktrace/Stacktrace.cpp
+++ src/driverapi/plugins/stacktrace/Stacktrace.cpp
@@ -303,22 +303,23 @@ extern "C" {
 
 		// addrOff[0] = possiblePreviousFrame;
 		// // 0x00007ffff5483eba
-		// Frame myFrame;
-		// Frame * fr = Dyninst::Stackwalker::Frame::newFrame(0x7ffff5c54eba, 0x7fffffffb820, 0x733c18, local_walker);
-		// std::cerr << std::hex << fr->getRA() << std::dec << " " << std::hex << fr->getSP() << std::dec << " " << std::hex << fr->getFP() << std::dec << " " << std::endl;
-		// myFrame = *fr;
+		Frame myFrame;
+		Frame * fr = Dyninst::Stackwalker::Frame::newFrame(returnAddress, dyninstRSP, prevRBP, local_walker);
+		std::cerr << std::hex << fr->getRA() << std::dec << " " << std::hex << fr->getSP() << std::dec << " " << std::hex << fr->getFP() << std::dec << " " << std::endl;
+		myFrame = *fr;
 		// // std::cerr << std::hex << myFrame.getRA() << std::dec << " " << std::hex << myFrame.getSP() << std::dec << " " << std::hex << myFrame.getFP() << std::dec << " " << std::endl;
 		
-		// // //local_walker->getInitialFrame(myFrame);
-		// for(int i = 0; i < 10; i++) {
-		// 	Frame out;
-		// 	if (local_walker->walkSingleFrame(myFrame, out) == false)
-		// 		break;
-		// 	std::string tmp;
-		// 	out.getName(tmp);
-		// 	std::cerr << tmp << " " << std::hex << out.getRA() << std::dec << " " << std::hex << out.getSP() << std::dec << " " << std::hex << out.getFP() << std::dec << " " << std::endl;
-		// 	myFrame = out;
-		// }
+		// //local_walker->getInitialFrame(myFrame);
+		for(int i = 0; i < 10; i++) {
+			Frame out;
+			if (local_walker->walkSingleFrame(myFrame, out) == false)
+				break;
+			std::string tmp;
+			out.getName(tmp);
+			std::cerr << tmp << " " << std::hex << out.getRA() << std::dec << " " << std::hex << out.getSP() << std::dec << " " << std::hex << out.getFP() << std::dec << " " << std::endl;
+			myFrame = out;
+		}
+		std::cerr << "End of stack" << std::endl;
 		// // std::cerr << "Done walking the stack" << std::endl;
 		// // //0x7ffff6280388
 

79fc1e71be17c403014810aa1200be5e9535a872
Benjamin Welton <welton@cs.wisc.edu>
2018-04-11 14:27:26 -0500

    Changes

--- src/driverapi/launcher/StacktraceInst.cpp
+++ src/driverapi/launcher/StacktraceInst.cpp
@@ -115,7 +115,7 @@ void StacktraceInst::Setup() {
 	_wrapSym = NULL;
 	for (auto i : tmp) {
 		if (i->getPrettyName().find("SynchronizationWrapper") != std::string::npos) {
-			if (i->getModule()->fileName.find("STraceWrapSync") == std::string::npos)
+			if (i->getModule()->fileName().find("STraceWrapSync") == std::string::npos)
 				continue;
 			_wrapSym = i;
 			std::cerr << "Found synchtonization Wrapper" << std::endl;

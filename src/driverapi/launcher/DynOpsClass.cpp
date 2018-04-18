8e3fff63a15029dd1aefd84e31f421e9737ac182
Benjamin Welton <welton@cs.wisc.edu>
2018-04-18 16:41:12 -0500

    Added

---
+++ src/driverapi/launcher/DynOpsClass.cpp
@@ -0,0 +1,51 @@
+#include "DynOpsClass.h"
+int DynOpsClass::FindFuncByStackPoint(BPatch_addressSpace * aspace, BPatch_function * & ret, StackPoint & point) {
+	if (aspace == NULL) 
+		return -1;
+	int tmp = 0;
+	if (point.funcOffset > 0)
+		tmp = FindFuncByLibnameOffset(aspace, ret, point.libname, point.funcOffset);
+	if (tmp <= 0)
+		tmp = FindFuncByName(aspace, ret, point.funcName);
+
+	return tmp; 
+}
+
+int DynOpsClass::FindFuncByName(BPatch_addressSpace * aspace, BPatch_function * & ret, std::string name) {
+	if (aspace == NULL) 
+		return -1;
+	BPatch_image * img = _addrSpace->getImage();
+	std::vector<BPatch_function *> tmp;
+	_img->findFunction(name.c_str(), tmp);
+	if (tmp.size() > 0) 
+		ret = tmp[0];
+	return tmp.size();
+}
+
+BPatch_object * DynOpsClass::FindObjectByName(BPatch_addressSpace * aspace, std::string & name) {
+	if (aspace == NULL) 
+		return NULL;
+	BPatch_image * img = _addrSpace->getImage();
+	std::vector<BPatch_object *> objects;
+	img->getObjects(objects);
+	for (auto i : objects) 
+		if (i->pathName() == name)
+			return i;
+	return NULL;
+}
+
+int DynOpsClass::FindFuncByLibnameOffset(BPatch_addressSpace * aspace, BPatch_function * & ret, std::string libname, uint64_t offset) {
+	if (aspace == NULL) 
+		return -1;
+	BPatch_image * img = _addrSpace->getImage();
+	BPatch_object * obj = FindObjectByName(aspace, libname);
+	
+	if (obj == NULL)
+		return -1;
+
+	BPatch_function * func = img->findFunction(obj->fileOffsetToAddr(offset));
+	if (func == NULL)
+		return 0;
+	ret = func;
+	return 1;
+}

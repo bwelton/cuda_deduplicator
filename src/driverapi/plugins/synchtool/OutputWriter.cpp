2dbad3d20a9c26fbb144e80710fa4921f7d22e73
Benjamin Welton <welton@cs.wisc.edu>
2018-04-19 15:39:24 -0500

    Added

---
+++ src/driverapi/plugins/synchtool/OutputWriter.cpp
@@ -0,0 +1,22 @@
+#include "OutputWriter.h"
+
+OutputWriter::OutputWriter() : _curPos(1) {
+	_accessFile.reset(new OutputFile("firstAccesses.bin"));
+	_stackKeyFile.reset(new OutputFile("accessKeyFile.bin"));
+}
+
+void OutputWriter::RecordAccess(uint64_t id, std::vector<uint64_t> & currentStack) {
+	uint64_t hash = HashStack(currentStack);
+	if (_prevStacks.find(hash) == _prevStacks.end()) {
+		_stackKeyFile->Write(_curPos, currentStack);
+		_prevStacks[hash] = _curPos;
+	}
+	_accessFile->Write(id, _prevStacks[hash]);
+}
+
+uint64_t OutputWriter::HashStack(std::vector<uint64_t> & currentStack) {
+	std::stringstream ss;
+	for (auto i : currentStack)
+		ss << i << ",";
+	return std::hash<std::string>()(ss.str());	
+}

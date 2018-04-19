8dafc358de57fe6e96a234f16dae4fec5171003b
Benjamin Welton <welton@cs.wisc.edu>
2018-04-19 14:33:42 -0500

    Changes

---
+++ src/driverapi/plugins/synchtool/CheckAccesses.cpp
@@ -0,0 +1,16 @@
+CheckAccesses::CheckAccesses() {};
+
+void CheckAccesses::AddMemoryTransfer(MemoryRange & range) {
+	_current.push_back(range);
+}
+
+void CheckAccesses::SyncCalled() {
+	_prev = _current;
+}
+
+bool CheckAccesses::IsAddressProtected(uint64_t addr) {
+	for (auto i : _prev)
+		if(i.IsInRange(addr))
+			return true;
+	return false;
+}
---
+++ src/driverapi/plugins/synchtool/CheckAccesses.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <vector>
+#include <memory>
+#include "MemoryRange.h"
+
+class CheckAccesses {
+public:
+	CheckAccesses();
+	void AddMemoryTransfer(MemoryRange & range);
+	void SyncCalled();
+	bool IsAddressProtected(uint64_t addr);
+private:
+	std::vector<MemoryRange> _prev;
+	std::vector<MemoryRange> _current;	
+};
+
+typedef std::shared_ptr<CheckAccesses> CheckAccessesPtr;
+

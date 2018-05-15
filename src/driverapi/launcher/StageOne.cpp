a937b33a76e02289c49e859d650adad53f8e4862
Benjamin Welton <welton@cs.wisc.edu>
2018-05-15 17:11:26 -0500

    Added

--- src/driverapi/launcher/StageOne.cpp
+++ src/driverapi/launcher/StageOne.cpp
@@ -5,8 +5,8 @@ StageOne::StageOne(boost::program_options::variables_map vm) : _vm(vm) {
 }
 
 bool StageOne::Run() {
-	assert(_rw.OpenLibrary(std::string("libcuda.so.1")) == true);
-	assert(_rw.OpenLibrary(std::string(LOCAL_INSTALL_PATH) + std::string("/lib/plugins/libStacktrace.so")) == true);	
+	_rw.OpenLibrary(std::string("libcuda.so.1"));
+	_rw.OpenLibrary(std::string(LOCAL_INSTALL_PATH) + std::string("/lib/plugins/libStacktrace.so"));	
 	StacktraceInst inst(_rw.GetAppBinary()->GetAddressSpace(), _rw.GetAppBinary()->GetImage());
 	inst.InsertStackInst();
 	// Run application until completion

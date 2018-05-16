55a92cc5c807e6c5c3d39c3e68adf043449f9f61
Ben Welton <welton@cs.wisc.edu>
2018-05-15 23:49:24 -0500

    Added

--- src/driverapi/launcher/StageOne.h
+++ src/driverapi/launcher/StageOne.h
@@ -16,3 +16,23 @@ private:
 };
 
 
+// What each stage needs to output:
+// 1. Stage One:
+//    - NI_Dependencies.txt (\n seperated text list of libraries)
+//    - NI_Synchronizations.txt StackKeyWriter/Reader (key file from libstacktrace.so)
+// 2. Stage Two - Timing the synchroization calls:
+//     Required Inputs:
+//    - NI_Synchronizations.txt (keyfile from libstacktrace.so)
+//     Outputs:
+//    - TF_timekey.bin - StackKeyWriter/Reader
+//    - TF_trace.bin - TFReaderWriter output (synch: dynId -> TF_timerIDMap, stackId ->  TF_timekey.bin, count, time) 
+//    - TF_timerIdMap.txt (timerId,stackpoint)
+// 3. Stage Three - load store analysis 
+//    Required Inputs:
+//    - NI_Synchronizations.txt
+//    Instrimentation Outputs:
+//    - BM_library.key (Binary Location Map Reader/Writer)
+//    - BM_library.bin
+//    Runtime Outputs:
+//    - LS_StackKey.bin 
+//    - LS_Trace.bin

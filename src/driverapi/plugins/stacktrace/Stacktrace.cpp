4cbfd93a374f5c7ac0cb5236ef56e259e4cb713b
Benjamin Welton <welton@cs.wisc.edu>
2018-05-16 16:45:53 -0500

    Added

--- src/driverapi/plugins/stacktrace/Stacktrace.cpp
+++ src/driverapi/plugins/stacktrace/Stacktrace.cpp
@@ -55,7 +55,7 @@ extern "C" {
 	}
 	void INTERCEPT_DL_OPEN(const char * name) {
 		SETUP_LIBRARY_FILE();
-		std::cout << name << " was loaded" << std::endl;
+		std::cerr << name << " was loaded" << std::endl;
 		WriteLibraryCalls->WriteLibraryName(name);
 	}
 

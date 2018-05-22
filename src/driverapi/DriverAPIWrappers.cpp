eac5bcfc5c69821c91b7806ca571d279aa07e576
Benjamin Welton <welton@cs.wisc.edu>
2018-05-22 13:06:48 -0500

    More additions

--- src/driverapi/DriverAPIWrappers.cpp
+++ src/driverapi/DriverAPIWrappers.cpp
@@ -3063,9 +3063,9 @@ extern "C" void DefineBinders() {
 	Bound_cuInit = std::bind(&ORIGINAL_cuInit,std::placeholders::_1);
 	// int (tester) (int *);
 	// tester = (int (*)(int *))dlsym(handle, "cuDriverGetVersion_dyninst");
-	int (*tester)(int*) = (int (*)(int *))dlsym(handle, "SPICY_ORIGINAL_cuDriverGetVersion");
-	assert(tester != NULL);
-	//Bound_cuDriverGetVersion = std::bind(&ORIGINAL_cuDriverGetVersion,std::placeholders::_1);
+	// int (*tester)(int*) = (int (*)(int *))dlsym(handle, "SPICY_ORIGINAL_cuDriverGetVersion");
+	// assert(tester != NULL);
+	Bound_cuDriverGetVersion = std::bind(&ORIGINAL_cuDriverGetVersion,std::placeholders::_1);
 	Bound_cuDriverGetVersion = std::bind(tester,std::placeholders::_1);
 	Bound_cuDeviceGet = std::bind(&ORIGINAL_cuDeviceGet,std::placeholders::_1,std::placeholders::_2);
 	Bound_cuDeviceGetCount = std::bind(&ORIGINAL_cuDeviceGetCount,std::placeholders::_1);

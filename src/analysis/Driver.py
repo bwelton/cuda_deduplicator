c6ea58a8d47acfe53277c0bb289dbe73a1fe4d83
Ben Welton <welton@cs.wisc.edu>
2018-05-24 01:12:39 -0500

    More changes to analysis scripts

--- src/analysis/Driver.py
+++ src/analysis/Driver.py
@@ -1,20 +1,104 @@
 import sys
 import os
 from  ReadStackFiles import StackReader
+from LS_TraceBin import LS_TraceBin
+from TF_trace import TF_Trace
+
+class MatchTimeToLSStack:
+    def __init__(self, TF_timekey, LS_stackkey):
+        self._tf = TF_timekey
+        self._ls = LS_stackkey
+
+    def PruneTFStack(self, stack):
+        ret = stack._stack
+        ret = ret[2:]
+        return ret[:-1]
+
+
+    def CompareStacks(self, ls, tf):
+        #ls stack will always be longer
+        for x in range(0,len(tf)):
+            if str(ls[x]) != str(tf[x]):
+                return False
+        return True
+
+
+    def GetMatchSet(self):
+        # returns [(TFNum,LSNum)]
+        ret = []
+        tf_stacks = self._tf.GetAllStacks()
+        ls_stacks = self._ls.GetAllStacks()
+        for x in tf_stacks:
+            tmp = self.PruneTFStack(tf_stacks[x])
+            print tmp[-1]
+            for y in ls_stacks:
+                tmp2 = ls_stacks[y]._stack
+                if self.CompareStacks(tmp2, tmp):
+                    ret.append([tf_stacks[x]._ident,ls_stacks[y]._ident])
+        print ret
+        return ret
+
 class Driver:
     def __init__(self):
         self._inDir = sys.argv[1]
         self._outDir = sys.argv[2]
 
     def Run(self):
+        ## TIme in Seconds
+        TIME = 109.00
         self._stackStore = {}
+        ## LS_stackkey -> Necessary Synchronization
+        ## LS_tracekey -> Locations of first access
+        ## TF_timekey -> Timing for each synchronization.
         stack_files = ["LS_stackkey.txt", "LS_tracekey.txt", "TF_timekey.txt"]
 
         for x in stack_files:
             self._stackStore[x] = StackReader(os.path.join(self._inDir, x))
-
+        self._stackStore["TF_timekey.txt"].InverseStacks()
         for x in self._stackStore:
             print str(self._stackStore[x])
+
+        ## LS_trace Reader
+        ls_trace = LS_TraceBin(os.path.join(self._inDir, "LS_trace.bin"))
+        ls_trace.DecodeFile()
+        print str(ls_trace)
+
+        ## TF_Trace Decorder 
+        tf_trace = TF_Trace(os.path.join(self._inDir, "TF_trace.bin"))
+        tf_trace.DecodeFile()
+        print str(tf_trace)
+        match = MatchTimeToLSStack(self._stackStore["TF_timekey.txt"], self._stackStore["LS_stackkey.txt"])
+        m = match.GetMatchSet()
+        singleSetTF = []
+        for x in m:
+            singleSetTF.append(x[0])
+        notInSet = tf_trace.GetNotInSet(singleSetTF)
+        print notInSet
+        timeSavedEstimate = 0.0
+        for x in notInSet:
+            timeSavedEstimate += x[-1]
+        tmp_sort = {}
+        for x in notInSet:
+            if x[2] not in tmp_sort:
+                tmp_sort[x[2]] = x
+            else:
+                tmp_sort[x[2]][-1] += x[-1]
+                tmp_sort[x[2]][-2] += x[-2]
+        notInSet = []
+        for x in tmp_sort:
+            notInSet.append(tmp_sort[x])
+        print "\n\n\n\n"
+        print "Estimated time savings from unnecessary synchroniations/data transfers: " + str(timeSavedEstimate)
+        print "Table of unnecessary operation\n"
+        print '%-20.20s | %-20.20s | %-20.20s | %-10.10s | %-20.20s | %-10.10s | %-10.10s' % ('App Bin','App Function','Cuda Call','Stack ID','Type','Call Count', 'Est Impact')
+        for x in notInSet:
+            i = x[2]
+            stack = self._stackStore["TF_timekey.txt"].GetStackAtID(int(i))
+            ucall = stack.FindFirstUserCall()
+            ccall = stack.FindFirstLibCuda()
+            print '%-20.20s | %-20.20s | %-20.20s | %-10.10s | %-20.20s | %-10.10s | %-10.10s' % (ucall.GetFilename(),str(ucall._offset),str(ccall._offset),str(i),"Sync",str(x[3]), str(x[4]))
+
+
 
 
 if __name__ == "__main__":
--- src/analysis/ReadStackFiles.py
+++ src/analysis/ReadStackFiles.py
@@ -3,9 +3,17 @@ class StackEntry:
         self._libname = libname
         self._offset = offset
 
+
+    def __eq__(self, other):
+        if (str(self) == str(other)):
+            return True
+        return False
+
     def __str__(self):
         return str(self._libname) + "@" + str(self._offset) 
 
+    def GetFilename(self):
+        return self._libname.split("/")[-1]
 class Stack:
     def __init__(self, i):
         self._stack = []
@@ -13,11 +21,32 @@ class Stack:
     def AddEntry(self, libname, offset):
         self._stack.append(StackEntry(libname, offset))
 
+    def GetStack(self):
+        return self._stack
+
+    def FindFirstUserCall(self):
+        startPos = 0
+        for x in range(0,len(self._stack)):
+            if "libcuda.so" in  self._stack[x]._libname:
+                startPos = x
+                break
+        return self._stack[startPos-1]
+
+    def FindFirstLibCuda(self):
+        for x in self._stack:
+            if "libcuda.so" in  x._libname:
+                return x
     def __str__(self):
         ret = "\tStack " + str(self._ident)  + "\n"
         for i in self._stack:
             ret += "\t\t" + str(i) + "\n"
         return ret
+
+    def InverseStack(self):
+        tmp = []
+        for x in range(len(self._stack) - 1, 0, -1):
+            tmp.append(self._stack[x])
+        self._stack = tmp
 
 def RemoveNewline(line):
     return line.replace("\n","")
@@ -51,3 +80,7 @@ class StackReader:
 
     def GetAllStacks(self):
         return self._stacks
+
+    def InverseStacks(self):
+        for x in self._stacks:
+            self._stacks[x].InverseStack()

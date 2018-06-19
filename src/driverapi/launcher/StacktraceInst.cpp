e624c10206df90fc8998e59bbdf7f20b28a8be7a
Benjamin Welton <welton@cs.wisc.edu>
2018-06-18 17:00:05 -0700

    Added minor changes to dump image/core

--- src/driverapi/launcher/StacktraceInst.cpp
+++ src/driverapi/launcher/StacktraceInst.cpp
@@ -14,7 +14,7 @@ void StacktraceInst::InsertStackInst() {
 #ifdef USE_BPATCHINSERT
 	// Testing
 	//assert(cuCtxSync.size() > 0);
-	std::vector<BPatch_point*> * entryLocations = _cudaSync->findPoint(BPatch_locExit);
+	std::vector<BPatch_point*> * entryLocations = _cudaSync->findPoint(BPatch_locEntry);
 	std::vector<BPatch_snippet*> testArgs;
 	BPatch_funcCallExpr recordFuncEntry(*_wrapperFunc, testArgs);
 	assert(_addrSpace->insertSnippet(recordFuncEntry,*entryLocations) != false);

76e121ff97ceef4cd697d86c8a483f242a03a01a
Ben Welton <welton@cs.wisc.edu>
2018-07-14 02:20:50 -0500

    added

--- src/driverapi/launcher/SyncTesting.cpp
+++ src/driverapi/launcher/SyncTesting.cpp
@@ -26,60 +26,60 @@ void SyncTesting::ReprocessModel() {
 
 void SyncTesting::Run() {
 	double time;
-	// {
-	// 	TimeApplications base(_vm);
-	// 	std::cerr << "Running " << _programName << " without instrimentation to obtain total execution time" << std::endl;
-	// 	std::cerr << "Saving application output to file : " << _programName << ".base.out" << std::endl;
-	// 	//base.RedirectOutToFile(_programName + std::string(".base.out"));
-	// 	time = base.Run();
-	// 	//base.ReturnToTerminal();
-	// 	_model.AddExecutionTime(time);
-	// 	std::cerr << "Application executed with runtime of - " << time << "s" << std::endl;
-	// }
+	{
+		TimeApplications base(_vm);
+		std::cerr << "Running " << _programName << " without instrimentation to obtain total execution time" << std::endl;
+		std::cerr << "Saving application output to file : " << _programName << ".base.out" << std::endl;
+		//base.RedirectOutToFile(_programName + std::string(".base.out"));
+		time = base.Run();
+		//base.ReturnToTerminal();
+		_model.AddExecutionTime(time);
+		std::cerr << "Application executed with runtime of - " << time << "s" << std::endl;
+	}
 	
 	
-	// //RunWithCUPTI();
+	//RunWithCUPTI();
 
-	// // Find out what user called functions actually contain a synchronization.
-	// // This also captures secret entries into libcuda with synchronizations and relates them back
-	// // to user level calls. 
-	// {
-	// 	// Remove all previous stack files from this directory.
-	// 	system("exec rm -rf ./stackOut.*");
-	// 	TimeApplications base(_vm);
-	// 	std::cerr << "Running " << _programName << " with stacktrace information enabled" << std::endl;
-	// 	std::cerr << "Saving application output to file : " << _programName << ".stacktrace.out" << std::endl;
-	// 	//base.RedirectOutToFile(_programName + std::string(".stacktrace.out"));
-	// 	time = base.RunWithStackTracing();
-	// 	//base.ReturnToTerminal();
-	// 	_model.AddExecutionTime(time);
-	// 	std::cerr << "Application executed with runtime of - " << time << "s" << std::endl;		
-	// }
-	// //return;
-	//  _model.ReadStackFiles();
-	// //_model.ExtractLineInfo();
-	// //InstrumentProgram();
-	// std::vector<StackPoint> timingList;
-	// _model.GetTimingList(timingList);
-	// std::cout << "Timing list of size - " << timingList.size() << std::endl;
+	// Find out what user called functions actually contain a synchronization.
+	// This also captures secret entries into libcuda with synchronizations and relates them back
+	// to user level calls. 
+	{
+		// Remove all previous stack files from this directory.
+		system("exec rm -rf ./stackOut.*");
+		TimeApplications base(_vm);
+		std::cerr << "Running " << _programName << " with stacktrace information enabled" << std::endl;
+		std::cerr << "Saving application output to file : " << _programName << ".stacktrace.out" << std::endl;
+		//base.RedirectOutToFile(_programName + std::string(".stacktrace.out"));
+		time = base.RunWithStackTracing();
+		//base.ReturnToTerminal();
+		_model.AddExecutionTime(time);
+		std::cerr << "Application executed with runtime of - " << time << "s" << std::endl;		
+	}
+	//return;
+	 _model.ReadStackFiles();
+	//_model.ExtractLineInfo();
+	//InstrumentProgram();
+	std::vector<StackPoint> timingList;
+	_model.GetTimingList(timingList);
+	std::cout << "Timing list of size - " << timingList.size() << std::endl;
 
-	// // Get timing for these functions
-	// {
-	// 	TimeApplications base(_vm);
-	// 	std::cerr << "Running " << _programName << " with timing information enabled" << std::endl;
-	// 	std::cerr << "Saving application output to file : " << _programName << ".time.out" << std::endl;
-	// 	base.RedirectOutToFile(_programName + std::string(".time.out"));		
-	// 	double mytime = base.InternalRunWithTiming(timingList);
-	// 	base.ReturnToTerminal();
-	// 	_model.AddExecutionTime(mytime);	
-	// 	std::cerr << "Application executed with runtime of - " << mytime << "s" << std::endl;	
-	// }
+	// Get timing for these functions
+	{
+		TimeApplications base(_vm);
+		std::cerr << "Running " << _programName << " with timing information enabled" << std::endl;
+		std::cerr << "Saving application output to file : " << _programName << ".time.out" << std::endl;
+		base.RedirectOutToFile(_programName + std::string(".time.out"));		
+		double mytime = base.InternalRunWithTiming(timingList);
+		base.ReturnToTerminal();
+		_model.AddExecutionTime(mytime);	
+		std::cerr << "Application executed with runtime of - " << mytime << "s" << std::endl;	
+	}
 
 
-	// // Check that our timing run matches the synchronizations identified.
-	// // If so, this program is very likely deterministic given the same inputs in relation to cuda calls.
-	// _model.CaptureSyncTime();
-	// //return;
+	// Check that our timing run matches the synchronizations identified.
+	// If so, this program is very likely deterministic given the same inputs in relation to cuda calls.
+	_model.CaptureSyncTime();
+	//return;
 
 	// Load Store Instrimentation.
 	// What needs to be captured:
@@ -97,12 +97,11 @@ void SyncTesting::Run() {
 		//std::string def("");
 		TimeApplications base(_vm);
 		std::vector<std::tuple<std::string, std::string, std::string, std::string, std::string> > extras;
-		std::map<uint64_t, StackRecord> stackSyncs; //= _model.GetTimingStackRecords();
-		std::vector<StackPoint> timingList;
-		//std::map<uint64_t, StackRecord> stackSyncs = _model.GetTimingStackRecords();
+		std::map<uint64_t, StackRecord> stackSyncs = _model.GetTimingStackRecords();
+
 		//extras.push_back(std::make_tuple(std::string("wrap"), std::string(INTERNAL_SYNC), std::string("INTER_InternalSynchronization"), std::string(DRIVER_LIBRARY), std::string("ORIGINAL_InternalSynchronization")));
 		double time = base.RunWithLSInstrimentation(def, extras, timingList, stackSyncs);
-		//std::cerr << "[Base] We are setting up syncs checking with " << stackSyncs.size() << std::endl;
+		std::cerr << "[Base] We are setting up syncs checking with " << stackSyncs.size() << std::endl;
 		//_model.AddFirstUses(base._firstUses);
 	}
 	//_model.FixLSStackFiles();

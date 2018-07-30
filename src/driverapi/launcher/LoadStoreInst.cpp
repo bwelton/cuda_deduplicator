442fe0216ff0aff2dd27bd0c46fdc1b341fbca9b
Benjamin Welton <welton@cs.wisc.edu>
2018-07-30 13:37:32 -0700

    More diogenes fixes

--- src/driverapi/launcher/LoadStoreInst.cpp
+++ src/driverapi/launcher/LoadStoreInst.cpp
@@ -79,8 +79,8 @@ void LoadStoreInst::WrapEntryAndExit(std::map<uint64_t, StackRecord> & syncStack
 		for (auto z : points) {
 			_logFile << "[LoadStoreInst][EntryExit] Attempting to find - " << z.funcName << std::endl;
 			std::cerr << "[LoadStoreInst][EntryExit] Attempting to find - " << z.funcName << std::endl;
-			// if (z.funcName.find("targ") != std::string::npos)
-			// 	continue;
+			if (z.funcName.find("targ") != std::string::npos)
+				continue;
 			BPatch_function * func;
 			if(_dynOps.FindFuncByStackPoint(_addrSpace, func, z) <= 0){
 				_logFile << "[LoadStoreInst][EntryExit] Could not find function - " << z.funcName << std::endl;

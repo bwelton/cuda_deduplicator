bf8985c006f5039804d74b91e63033a38829b2e1
Benjamin Welton <welton@cs.wisc.edu>
2018-08-07 17:08:28 -0700

    Added

--- src/driverapi/launcher/LoadStoreInst.cpp
+++ src/driverapi/launcher/LoadStoreInst.cpp
@@ -135,7 +135,7 @@ void LoadStoreInst::InsertLoadStoreSnippets(BPatch_function * func, std::vector<
 			id = _binLoc.StorePosition(libname, libOffsetAddr);
 		else
 			id = _binLoc.StorePosition(libname, (uint64_t) i->getAddress());
-		if (libOffsetAddr == 0x102b3720 || libOffsetAddr == 0x102b3718)
+		if (libOffsetAddr == 0x102b3720 || libOffsetAddr == 0x102b3718 || libOffsetAddr == 0x102b36e0 || libOffsetAddr == 0x102b36c4)
 			continue;
 		std::vector<BPatch_snippet*> recordArgs;
 		BPatch_snippet * loadAddr = new BPatch_effectiveAddressExpr();

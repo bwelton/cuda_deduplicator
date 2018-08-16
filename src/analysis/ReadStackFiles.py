32b048c94da936a8b891b6283b3ab3856bb7be1a
Benjamin Welton <welton@cs.wisc.edu>
2018-08-16 15:20:46 -0700

    More additions

--- src/analysis/ReadStackFiles.py
+++ src/analysis/ReadStackFiles.py
@@ -89,7 +89,7 @@ class Stack:
                 index = int(count,16) - 1
                 a = tmp[1].split("@")
                 if self._stack[index]._libname == a[0] and int(self._stack[index]._offset) == int(a[2],16):
-                    self._funcname = a[1]
+                    self._stack[index]._funcname = a[1]
                 else:
                     print "NON MATCH"
                     print str(self._stack[index])

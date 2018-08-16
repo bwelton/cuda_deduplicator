c6e75ada6b100c41e6ecf527ee63a1b54e26100a
Benjamin Welton <welton@cs.wisc.edu>
2018-08-16 15:55:32 -0700

    More additions

--- src/analysis/ReadStackFiles.py
+++ src/analysis/ReadStackFiles.py
@@ -94,7 +94,7 @@ class Stack:
                 index = int(count,16) - 1
                 a = tmp[1].split("@")
                 if self._stack[index]._libname == a[0] and int(self._stack[index]._offset) == int(a[2],16):
-                    lines = os.popen("/usr/bin/c++filt " + a[1]).read()
+                    lines = os.popen("/usr/bin/c++filt " + a[1]).read().splitlines()
                     print lines
                     self._stack[index]._funcname = a[1]
                     #print a[1]

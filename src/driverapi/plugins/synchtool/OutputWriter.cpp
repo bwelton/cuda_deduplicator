a52ca675bc34fb6489b13b39bf2b13bde3e89b46
Benjamin Welton <welton@cs.wisc.edu>
2018-08-20 11:21:56 -0700

    Testing StackWalker

--- src/driverapi/plugins/synchtool/OutputWriter.cpp
+++ src/driverapi/plugins/synchtool/OutputWriter.cpp
@@ -2,16 +2,17 @@
 
 OutputWriter::OutputWriter() : _curPos(1) {
 	_accessFile.reset(new OutputFile(std::string("LS_trace.bin")));
-	_stackKeyFile.reset(new OutputFile(std::string("LS_stackkey.bin")));
+	_stackKeyFile.reset(new StackKeyWriter(std::string("LS_stackkey.bin")));
 }
 
-void OutputWriter::RecordAccess(uint64_t id, std::vector<uint64_t> & currentStack) {
-	uint64_t hash = HashStack(currentStack);
-	if (_prevStacks.find(hash) == _prevStacks.end()) {
-		_stackKeyFile->Write(_curPos, currentStack);
-		_prevStacks[hash] = _curPos;
-	}
-	_accessFile->Write(id, _prevStacks[hash]);
+void OutputWriter::RecordAccess(uint64_t id, std::vector<StackPoint> & currentStack) {
+	// uint64_t hash = HashStack(currentStack);
+	// if (_prevStacks.find(hash) == _prevStacks.end()) {
+	// 	_stackKeyFile->Write(_curPos, currentStack);
+	// 	_prevStacks[hash] = _curPos;
+	// }
+	uint64_t idKey = _stackKeyFile->InsertStack(currentStack);
+	_accessFile->Write(id, idKey);
 }
 
 uint64_t OutputWriter::HashStack(std::vector<uint64_t> & currentStack) {

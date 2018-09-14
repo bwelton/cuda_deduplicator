33adb34a75364f6d6624c97aa7e69beec3385ba4
Benjamin Welton <welton@cs.wisc.edu>
2018-09-14 18:46:20 -0500

    More changes

--- src/driverapi/launcher/LoadStoreInst.cpp
+++ src/driverapi/launcher/LoadStoreInst.cpp
@@ -202,6 +202,23 @@ void LoadStoreInst::InsertLoadStoreInstrimentation() {
 			InsertLoadStoreSnippets(i, loadsAndStores);
 		}
 	}
+
+	// Now lets add the special cases
+	// Start at -1 from limit max
+	std::string specModule = std::string("libc.so.6");
+	BPatch_object * specObj = _dynOps.FindObjectByName(_addrSpace,  specModule, false);
+	uint64_t specID = std::numeric_limits<uint64_t>::max() - 2;
+	assert(specObj != NULL);
+	std::vector<BPatch_function *> specFuncs;
+	specObj->findFunction("memcpy",specFuncs,false);
+	assert(specFuncs.size() == 1);
+	BPatch_function * memcpyFunc = _dynOps.GetPOWERFunction(specFuncs[0]);
+	uint64_t libOffset = 0;
+	_dynOps.GetFileOffset(_addrSpace, memcpyFunc, libOffset);
+	std::string specObjPath = specObj->pathName();
+	_binLoc.SetAbsoluteID(specID, specObjPath, libOffset);
+
+	//specObj->pathName()
 }
 
 void LoadStoreInst::FixLSStackFiles() {

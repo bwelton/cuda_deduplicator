df0bd29276d551f8d6b3de23bf3826a7eb6dbd55
Benjamin Welton <welton@cs.wisc.edu>
2018-10-11 17:43:58 -0500

    More minor changes

--- src/driverapi/launcher/Common.h
+++ src/driverapi/launcher/Common.h
@@ -4,7 +4,7 @@
 #include <dirent.h>
 #include <string>
 #include <map>
-#include <stringstream>
+#include <sstream> 
 
 void StringSplit(const std::string &s, char delim, std::vector<std::string> & result);
 void GetFilesInDirectory(std::string & dir, std::vector<std::string> & files);

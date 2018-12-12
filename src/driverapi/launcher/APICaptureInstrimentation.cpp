0186655b94edba50bba59bdc7839c3df874fb986
Benjamin Welton <welton@cs.wisc.edu>
2018-12-12 14:41:11 -0600

    More conversion mods

--- src/driverapi/launcher/APICaptureInstrimentation.cpp
+++ src/driverapi/launcher/APICaptureInstrimentation.cpp
@@ -19,6 +19,7 @@ bool APICaptureInstrimentation::InsertInstrimentation() {
 	_proc->LoadLibrary(std::string(LOCAL_INSTALL_PATH) + std::string("/lib/plugins/libDriverAPITrace.so"));
 	_proc->LoadLibrary(std::string(LOCAL_INSTALL_PATH) + std::string("/lib/plugins/libDataTransfer.so"));
 	_proc->LoadLibrary(std::string(LOCAL_INSTALL_PATH) + std::string("/lib/plugins/libDataSyncTimer.so"));
+	_proc->LoadLibrary(std::string(LOCAL_INSTALL_PATH) + std::string("/lib/plugins/libSynchTool.so"));
 	std::vector<BPatch_function *> binderFunctions = ops->FindFuncsByName(_proc->GetAddressSpace(), std::string("DefineBinders"), driverAPIWrapper);
 
 	// We expect only a single call with this name, fail if there is more than one.

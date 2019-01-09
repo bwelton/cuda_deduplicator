ab82cfb7e5f7806023356bdb83f80df880398b09
Ben Welton <welton@cs.wisc.edu>
2019-01-09 00:28:01 -0600

    Added more things

---
+++ src/analysis/View1.py
@@ -0,0 +1,26 @@
+from Driver2 import JSStack, JSStackEntry
+import json
+
+## View 1: Top Level View
+#  cudaCall callCount %unnecessary estimated time savings
+cudaFunctions = os.path.join(os.path.dirname(os.path.realpath(__file__)),"cudaFunctions.txt")
+f = open(cudaFunctions, "r")
+data = f.readlines()
+f.close()
+
+def RemoveNewline(line):
+    return line.replace("\n","")
+
+cudaCalls = {}
+for x in data:
+	cudaCalls[RemoveNewline(x)] = 1
+
+f = open("combined_stacks.json", "rb")
+data = json.load(f)
+
+stacks = []
+for x in data:
+	stacks.append(JSStack(data=x))
+	
+
+

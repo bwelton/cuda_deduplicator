9bc27d884936c3dd4782629aa0dc420ad28d4267
Benjamin Welton <welton@cs.wisc.edu>
2019-02-07 15:32:28 -0600

    More changes

--- src/driverapi/launcher/DyninstFunctionWraps.cpp
+++ src/driverapi/launcher/DyninstFunctionWraps.cpp
@@ -18,7 +18,7 @@ void OneTimeFillMap() {
 	DFW_MAP[tmp.wrap] = tmp;
 
 	tmp = {
-		.wrap =  std::string("__GI_memcpy"),
+		.wrap =  std::string("__memcpy_power7"),
 		.library = std::string("libc-2.17.so"),
 		.wrapperName = std::string("diogenes_memcpy_wrapper"),
 		.argMap = {std::make_pair(0,0),std::make_pair(1,1),std::make_pair(2,2)},

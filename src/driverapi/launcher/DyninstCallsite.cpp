f9924e4055d8a01b4cebd72ce3cb0136008cb21c
Benjamin Welton <welton@cs.wisc.edu>
2019-04-23 16:29:07 -0500

    Added

---
+++ src/driverapi/launcher/DyninstCallsite.cpp
@@ -0,0 +1,21 @@
+#include "DyninstCallsite.h"
+DyninstCallsite::DyninstCallsite(std::shared_ptr<DyninstProcess> proc, DyninstFunction * func, BPatch_point & point) : _proc(proc), _func(func), _point(point), _calledFunc(std::string("UNKNOWN")), _replaced(false) {
+	BPatch_function * tmp = point.getCalledFunction();
+	if (tmp == NULL)
+		return;
+
+	_calledFunc = tmp->getName();
+}
+std::string * DyninstCallsite::GetCalledFunction() {
+	return &_calledFunc;
+}
+void DyninstCallsite::ReplaceFunctionCall(BPatch_function * _newCall) {
+	if (_replaced)
+		return;
+
+	_replaced = true;
+
+	BPatch_addressSpace * aspace = _proc->GetAddressSpace();
+
+	aspace->replaceFunctionCall(_point, *_newCall);
+}

cb2678f611538a8393e20fc4d3def59238ae7c99
Ben Welton <welton@cs.wisc.edu>
2019-04-24 18:56:43 -0500

    Added

--- src/driverapi/launcher/FixCudaFree.cpp
+++ src/driverapi/launcher/FixCudaFree.cpp
@@ -4,6 +4,7 @@ FixCudaFree::FixCudaFree(std::shared_ptr<DyninstProcess> proc) : _proc(proc) {}
 void FixCudaFree::InsertAnalysis(StackRecMap & recs) {
 	_bmap.reset(new BinaryLocationIDMap());
 	BPatch_object * libcuda = _proc->LoadLibrary(std::string("libcuda.so.1"));
+	BPatch_object * wrapper = _proc->LoadLibrary(std::string(LOCAL_INSTALL_PATH) + std::string("/lib/plugins/libDiogenesMemManager.so"));
 	std::shared_ptr<InstrimentationTracker> tracker(new InstrimentationTracker());
 	std::vector<BPatch_function *> all_functions;
 	BPatch_image * img = _proc->GetAddressSpace()->getImage();
@@ -19,6 +20,8 @@ void FixCudaFree::InsertAnalysis(StackRecMap & recs) {
 		if (i.second->IsExcludedFunction(LOAD_STORE_INST))
 			continue;
 		i.second->GetFuncInfo(tmpLibname, tmpFuncName);
+		if (tmpFuncName.find("DIOGENES_") != std::string::npos || tmpLibname.find("/lib/plugins/") != std::string::npos)
+			continue;
 		if (tmpLibname.find("/usr/lib64/libc-2.17.so") != std::string::npos)
 			continue;
 
@@ -32,6 +35,8 @@ void FixCudaFree::InsertAnalysis(StackRecMap & recs) {
 					std::cerr << "Found function call to cudaMalloc in " << tmpFuncName << " within library " << tmpLibname << " (calling " << *(x.GetCalledFunction()) << ")" << std::endl;
 				if (*(x.GetCalledFunction()) == std::string("__GI___libc_malloc"))
 					std::cerr << "Found function call to malloc in " << tmpFuncName << " within library " << tmpLibname << " (calling " << *(x.GetCalledFunction()) << ")" << std::endl;
+				if (*(x.GetCalledFunction()) == std::string("cudaMemcpyAsync"))
+					std::cerr << "Found function call to cudaMemcpyAsync in " << tmpFuncName << " within library " << tmpLibname << " (calling " << *(x.GetCalledFunction()) << ")"  << std::endl;
 			}
 		//}
 	}
--- src/driverapi/plugins/memmanager/MemManager.cpp
+++ src/driverapi/plugins/memmanager/MemManager.cpp
@@ -4,9 +4,22 @@
 #include <map>
 // CUDA include
 #include <cuda.h>
-
-CUresult cudaMemFreeWrapper(CUdeviceptr mem) {
+extern "C" {
+cudaError_t  DIOGENES_cudaFreeWrapper(void * mem) {
 	std::cerr << "I am freeing an address of .... " << std::hex << (uint64_t) mem << std::endl;
-	return cuMemFree(mem);
+	return cudaFree(mem);
+}
+
+cudaError_t DIOGENES_cudaMallocWrapper(void ** mem, size_t size) {
+	cudaError_t tmp = cudaMalloc(mem, size);
+	if (tmp == cudaSuccess)
+		std::cerr << "I alloced an address at " << std::hex << *((uint64_t**)mem)  <<  " of size " << size << std::endl;
+	return tmp;
+}
+
+cudaError_t DIOGENES_cudaMemcpyAsyncWrapper(void * dst, const void * src, size_t size, cudaMemcpyKind kind, cudaStream_t stream) {
+	std::cerr << "Initiating a transfer between  " << std::hex << dst <<  " and " << std::hex << src << " of size " << size << std::endl;
+	return cudaMemcpyAsync(dst, src, size, kind, stream);
+}
 }
 

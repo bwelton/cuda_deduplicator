15bc0115e2b3131cd6e9a99f413b1b8c7646163e
Ben Welton <welton@cs.wisc.edu>
2019-04-24 18:58:33 -0500

    Added

--- src/driverapi/plugins/memmanager/MemManager.cpp
+++ src/driverapi/plugins/memmanager/MemManager.cpp
@@ -3,21 +3,22 @@
 #include <string>
 #include <map>
 // CUDA include
+#include <cuda_runtime_api.h>
 #include <cuda.h>
 extern "C" {
-int  DIOGENES_cudaFreeWrapper(void * mem) {
+cudaError_t  DIOGENES_cudaFreeWrapper(void * mem) {
 	std::cerr << "I am freeing an address of .... " << std::hex << (uint64_t) mem << std::endl;
 	return cudaFree(mem);
 }
 
-int DIOGENES_cudaMallocWrapper(void ** mem, size_t size) {
+cudaError_t DIOGENES_cudaMallocWrapper(void ** mem, size_t size) {
 	cudaError_t tmp = cudaMalloc(mem, size);
 	if (tmp == cudaSuccess)
 		std::cerr << "I alloced an address at " << std::hex << *((uint64_t**)mem)  <<  " of size " << size << std::endl;
 	return tmp;
 }
 
-int DIOGENES_cudaMemcpyAsyncWrapper(void * dst, const void * src, size_t size, cudaMemcpyKind kind, cudaStream_t stream) {
+cudaError_t DIOGENES_cudaMemcpyAsyncWrapper(void * dst, const void * src, size_t size, cudaMemcpyKind kind, cudaStream_t stream) {
 	std::cerr << "Initiating a transfer between  " << std::hex << dst <<  " and " << std::hex << src << " of size " << size << std::endl;
 	return cudaMemcpyAsync(dst, src, size, kind, stream);
 }

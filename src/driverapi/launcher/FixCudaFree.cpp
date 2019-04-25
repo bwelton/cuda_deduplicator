2f11f862c11246384435d093e795e0eb41b98f89
Benjamin Welton <welton@cs.wisc.edu>
2019-04-25 15:06:11 -0500

    Added

--- src/driverapi/launcher/FixCudaFree.cpp
+++ src/driverapi/launcher/FixCudaFree.cpp
@@ -45,11 +45,11 @@ void FixCudaFree::InsertAnalysis(StackRecMap & recs) {
 			i.second->GetCallsites(callsites);
 			for (auto x : callsites) {
 				if (*(x.GetCalledFunction()) == std::string("cudaFree")){
-					//if (x.GetPointAddress() == (uint64_t) 0x10009364) {
+					if (x.GetPointAddress() == (uint64_t) 0x10006cc4) {
 						x.ReplaceFunctionCall(cudaFreeWrapper[0]);
 						std::cerr << "Found function call to cudaFree in " << tmpFuncName << " within library " << tmpLibname << " (calling " << *(x.GetCalledFunction()) << ")"  << std::endl;
-					break;
-					//}
+						return;
+					}
 				}
 				if (*(x.GetCalledFunction()) == std::string("cudaMalloc"))
 					std::cerr << "Found function call to cudaMalloc in " << tmpFuncName << " within library " << tmpLibname << " (calling " << *(x.GetCalledFunction()) << ")" << std::endl;

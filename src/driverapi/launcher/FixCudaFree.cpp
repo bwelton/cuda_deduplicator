861296e03f15ad5ff541ed02b9ed6098918b1041
Benjamin Welton <welton@cs.wisc.edu>
2019-04-25 14:32:27 -0500

    Added

--- src/driverapi/launcher/FixCudaFree.cpp
+++ src/driverapi/launcher/FixCudaFree.cpp
@@ -30,7 +30,7 @@ void FixCudaFree::InsertAnalysis(StackRecMap & recs) {
 	std::string binary_name = std::string("main");
 	std::string tmpLibname = std::string("");
 	std::string tmpFuncName = std::string("");
-	_proc->BeginInsertionSet();
+	//_proc->BeginInsertionSet();
 	for (auto i : _dyninstFunctions) {
 		if (i.second->IsExcludedFunction(LOAD_STORE_INST))
 			continue;
@@ -48,6 +48,7 @@ void FixCudaFree::InsertAnalysis(StackRecMap & recs) {
 					//if (x.GetPointAddress() == (uint64_t) 0x10009364) {
 						x.ReplaceFunctionCall(cudaFreeWrapper[0]);
 						std::cerr << "Found function call to cudaFree in " << tmpFuncName << " within library " << tmpLibname << " (calling " << *(x.GetCalledFunction()) << ")"  << std::endl;
+					break;
 					//}
 				}
 				if (*(x.GetCalledFunction()) == std::string("cudaMalloc"))

86bc61e9f3b185c87fdc0efed4417efe7d7e4943
Ben Welton <welton@cs.wisc.edu>
2019-05-29 17:00:01 -0500

    More changes

--- src/driverapi/launcher/StacktraceSynchronizations.cpp
+++ src/driverapi/launcher/StacktraceSynchronizations.cpp
@@ -226,6 +226,6 @@ void StacktraceSynchronizations::ReadResults(StackRecMap & recs) {
 		}
 	}
 	std::cerr << "[StacktraceSynchronizations::ReadResults] Size of output map = " << recs.size() << std::endl;
-	assert(recs.size() == 0);
-	
+	assert(recs.size() >= 0);
+
 }

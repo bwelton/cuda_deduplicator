9ee03bed1ab53c27edda4bb3e1f4fca8fd5e69c6
Ben Welton <welton@cs.wisc.edu>
2019-05-30 18:57:34 -0500

    More changes

---
+++ src/driverapi/launcher/MemRecorder.cpp
@@ -0,0 +1,105 @@
+#include "MemRecorder.h"
+#include <fstream>
+#include <sstream>
+#include <set>
+
+MemRecorder::MemRecorder(std::shared_ptr<DyninstProcess> proc) : _proc(proc) {}
+
+void MemRecorder::InsertAnalysis(StackRecMap & recs) {
+	DebugInstrimentationTemp debugOutput(std::string("DIOGENES_limitFunctions.txt"), std::string("DIOGENES_funcsInstrimented.txt"));
+
+	_bmap.reset(new BinaryLocationIDMap());
+	std::shared_ptr<DynOpsClass> ops = _proc->ReturnDynOps();
+	BPatch_object * libcuda = _proc->LoadLibrary(std::string("libcuda.so.1"));
+	BPatch_object * wrapper = _proc->LoadLibrary(std::string(LOCAL_INSTALL_PATH) + std::string("/lib/plugins/libDiogenesMemRecorder.so"));
+
+	std::vector<BPatch_function *> cudaFreeWrapper = ops->FindFuncsByName(_proc->GetAddressSpace(), std::string("DIOGENES_REC_CudaFree"), wrapper);
+	std::vector<BPatch_function *> cudaMallocWrapper = ops->FindFuncsByName(_proc->GetAddressSpace(), std::string("DIOGENES_REC_CudaMalloc"), wrapper);
+	std::vector<BPatch_function *> cudaMemcpyWrapper = ops->FindFuncsByName(_proc->GetAddressSpace(), std::string("DIOGENES_REC_CudaMemcpy"), wrapper);
+	std::vector<BPatch_function *> mallocWrapper = ops->FindFuncsByName(_proc->GetAddressSpace(), std::string("DIOGENES_REC_MALLOCWrapper"), wrapper);
+	std::vector<BPatch_function *> freeWrapper = ops->FindFuncsByName(_proc->GetAddressSpace(), std::string("DIOGENES_REC_FREEWrapper"), wrapper);
+
+	assert(cudaFreeWrapper.size() > 0);
+	assert(cudaMallocWrapper.size() > 0);
+	assert(cudaMemcpyWrapper.size() > 0);
+	assert(mallocWrapper.size() > 0);
+	assert(freeWrapper.size() > 0);
+	std::shared_ptr<InstrimentationTracker> tracker(new InstrimentationTracker());
+	std::vector<BPatch_function *> all_functions;
+	BPatch_image * img = _proc->GetAddressSpace()->getImage();
+	img->getProcedures(all_functions);
+	for (auto i : all_functions) {
+		_dyninstFunctions[(uint64_t)i->getBaseAddr()] = std::shared_ptr<DyninstFunction>(new DyninstFunction(_proc, i, tracker, _bmap));
+	}
+
+	std::string tmpLibname = std::string("");
+	std::string tmpFuncName = std::string("");
+	_proc->BeginInsertionSet();
+	int64_t ident = 2;
+	for (auto i : _dyninstFunctions) {
+		i.second->GetFuncInfo(tmpLibname, tmpFuncName);
+
+		if (tmpFuncName.find("DIOGENES_") != std::string::npos || tmpLibname.find("/lib/plugins/") != std::string::npos)
+			continue;
+		if (tmpLibname.find("/usr/lib64/libc-2.17.so") != std::string::npos || tmpLibname.find("libcudart") != std::string::npos || tmpLibname.find("libcuda") != std::string::npos)
+			continue;
+		
+		//if (tmpLibname.find(binary_name) != std::string::npos) {
+			std::vector<DyninstCallsite> callsites;
+			i.second->GetCallsites(callsites);
+			for (auto x : callsites) {
+				//std::cerr << "[DB]CS Function Name - " << *(x.GetCalledFunction()) << std::endl;
+				if (*(x.GetCalledFunction()) == std::string("cudaFree")){
+/*					if (!debugOutput.InstrimentFunction(tmpLibname, tmpFuncName,x.GetPointFileAddress()))
+						continue;
+					//if (x.GetPointAddress() == (uint64_t) 0x10006cc4) {
+					std::cerr << "[MemRecorder::InsertAnalysis] Found function call to cudaFree in " << tmpFuncName << " within library " << tmpLibname << " (calling " << *(x.GetCalledFunction()) << ")"  << std::endl;
+					x.ReplaceFunctionCall(cudaFreeWrapper[0]);
+*/
+					//return;
+					
+					//}
+				}
+				if (*(x.GetCalledFunction()) == std::string("cudaMalloc")) {
+/*					if (!debugOutput.InstrimentFunction(tmpLibname, tmpFuncName,x.GetPointFileAddress()))
+						continue;
+					std::cerr << "[MemRecorder::InsertAnalysis] Found function call to cudaMalloc in " << tmpFuncName << " within library " << tmpLibname << " (calling " << *(x.GetCalledFunction()) << ")" << std::endl;
+					x.ReplaceFunctionCall(cudaMallocWrapper[0]);
+					*/
+					//return;
+				}
+				if (*(x.GetCalledFunction()) == std::string("__GI___libc_malloc")){
+					if (!debugOutput.InstrimentFunction(tmpLibname, tmpFuncName,x.GetPointFileAddress()))
+						continue;
+					x.ReplaceFunctionCallWithID(mallocWrapper[0], ident);
+					_idToStackPoint[ident] = x.GetStackPoint();
+					ident++;
+					std::cerr << "[MemRecorder::InsertAnalysis] Found function call to malloc in " << tmpFuncName << " within library " << tmpLibname << " (calling " << *(x.GetCalledFunction()) << ")" << std::endl;
+				}
+				if (*(x.GetCalledFunction()) == std::string("__libc_free")){
+					if (!debugOutput.InstrimentFunction(tmpLibname, tmpFuncName,x.GetPointFileAddress()))
+						continue;
+					x.ReplaceFunctionCallWithID(freeWrapper[0], ident);
+					_idToStackPoint[ident] = x.GetStackPoint();
+					ident++;
+					std::cerr << "[MemRecorder::InsertAnalysis] Found function call to free in " << tmpFuncName << " within library " << tmpLibname << " (calling " << *(x.GetCalledFunction()) << ")" << std::endl;
+				}
+				if (*(x.GetCalledFunction()) == std::string("cudaMemcpyAsync")){
+/*					if (!debugOutput.InstrimentFunction(tmpLibname, tmpFuncName,x.GetPointFileAddress()))
+						continue;
+					std::cerr << "[MemRecorder::InsertAnalysis] Found function call to cudaMemcpyAsync in " << tmpFuncName << " within library " << tmpLibname << " (calling " << *(x.GetCalledFunction()) << ")"  << std::endl;
+					x.ReplaceFunctionCall(cudaMemcpyWrapper[0]);
+					*/
+				}
+			}
+		//}
+	}
+	// std::vector<BPatch_function*> cudaSyncFunctions = ops->GetFunctionsByOffeset(_proc->GetAddressSpace(), libcuda, ops->GetSyncFunctionLocation());
+	// assert(cudaSyncFunctions.size() > 0);
+
+	// std::vector<BPatch_point*> * entryPoints = cudaSyncFunctions[0]->findPoint(BPatch_locExit);
+	// std::vector<BPatch_snippet*> recordArgs;
+	// BPatch_funcCallExpr entryExpr(*(syncExit[0]), recordArgs);	
+	// assert(_proc->GetAddressSpace()->insertSnippet(entryExpr,*entryPoints) != NULL);
+	// 
+}

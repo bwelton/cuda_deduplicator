a7704b2aa1085fa2d0b28c8c984b7636f04358b2
Ben Welton <welton@cs.wisc.edu>
2019-05-31 17:07:20 -0500

    More changes

--- src/driverapi/launcher/CallTransformation.cpp
+++ src/driverapi/launcher/CallTransformation.cpp
@@ -1,7 +1,11 @@
 #include "CallTransformation.h"
 
 CallTransformation::CallTransformation(GPUMallocVec & gpuVec,CPUMallocVec & cpuVec, 
-	MemTransVec & memVec, std::map<int64_t, StackPoint> & idPoints) : _gpuVec(gpuVec), _cpuVec(cpuVec), _memVec(memVec), _idPoints(idPoints) { }
+	MemTransVec & memVec, std::map<int64_t, StackPoint> & idPoints) : _gpuVec(gpuVec), _cpuVec(cpuVec), _memVec(memVec), _idPoints(idPoints) {
+
+	BuildGraph();
+
+}
 
 
 void CallTransformation::BuildGraph() {

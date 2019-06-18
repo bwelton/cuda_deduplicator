53efa813ad0b54893806effece44b9faf864c561
Benjamin Welton <welton@cs.wisc.edu>
2019-06-18 15:33:18 -0500

    More changes

--- src/driverapi/plugins/memmanager/MemManager.cpp
+++ src/driverapi/plugins/memmanager/MemManager.cpp
@@ -631,13 +631,12 @@ void DIOGENES_FREEWrapper(void * mem) {
 		free(mem);
 		return;
 	}
-	if(DIOGENES_Atomic_Malloc.compare_exchange_weak(setVal, true)) {
+	if(DIOGENES_Atomic_Malloc.compare_exchange_weak(setVal, true) && DIOGENES_MEMMANGE_TEAR_DOWN == false) {
 		PLUG_BUILD_FACTORY()
 		DIOGENES_TRANSFER_MEMMANGE->ReleaseMemory(mem);
 		//PLUG_FACTORY_PTR->CPUFree(mem);
 		DIOGENES_Atomic_Malloc.exchange(false);
 	} else {
-
 		DIOGENES_LIBCFREE(mem);
 	}
 }

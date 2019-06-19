cb490bd1fc813e4f5fb4e21f4722a7ec685a7190
Ben Welton <welton@cs.wisc.edu>
2019-06-18 22:57:30 -0500

    Added

--- src/driverapi/plugins/memmanager/MemManager.cpp
+++ src/driverapi/plugins/memmanager/MemManager.cpp
@@ -762,15 +762,14 @@ void DIOGENES_FREEWrapper(void * mem) {
 			DIOGENES_LIBCFREE(mem);
 			return;
 		}
-		//if (DIOGENES_MUTEX_MANAGER->EnterFree()) {
+		if (DIOGENES_MUTEX_MANAGER->EnterFree()) {
 			DIOGENES_TRANSFER_MEMMANGE->ReleaseMemory(mem);
-		//	DIOGENES_MUTEX_MANAGER->ExitFree();
-		//} //else {
-		//	DIOGENES_LIBCFREE(mem);
-		// 	// if (DIOGENES_LIBCFREE != NULL){
-			
-		// 	// }
-		//}
+			DIOGENES_MUTEX_MANAGER->ExitFree();
+		} else {
+		 	if (DIOGENES_LIBCFREE != NULL){
+				DIOGENES_LIBCFREE(mem);
+			}
+		}
 	} 
 }
 }

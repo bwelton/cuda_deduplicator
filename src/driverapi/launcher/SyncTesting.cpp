7d76284904538e69c4042301a41158ee85ada268
Ben Welton <welton@cs.wisc.edu>
2019-10-09 16:12:47 -0500

    Added

--- src/driverapi/launcher/SyncTesting.cpp
+++ src/driverapi/launcher/SyncTesting.cpp
@@ -329,8 +329,8 @@ void SyncTesting::RunAutoCorrect() {
 
 void SyncTesting::IndentifySyncFunction() {
 	LocateCudaSynchronization scuda;
-	//if (scuda.FindLibcudaOffset(false) != 0)
-	//	return;
+	if (scuda.FindLibcudaOffset(false) != 0)
+		return;
 	std::vector<uint64_t> potentials = scuda.IdentifySyncFunction();
 	{
 		std::shared_ptr<DyninstProcess> proc = LaunchApplicationByName(std::string("/g/g17/welton2/scratch/nfs/apps/cumf_als/hang_devsynch"), false);

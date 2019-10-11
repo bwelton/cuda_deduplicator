6b1cb04bd7500bc02811fcded1338f1a2e4989f6
Ben Welton <welton@cs.wisc.edu>
2019-10-11 01:26:10 -0500

    Added

--- src/driverapi/launcher/DynOpsClass.cpp
+++ src/driverapi/launcher/DynOpsClass.cpp
@@ -75,7 +75,7 @@ int DynOpsClass::FindFuncByStackPoint(BPatch_addressSpace * aspace, BPatch_funct
 }
 
 bool DynOpsClass::IsNeverInstriment(BPatch_function * func) {
-	static std::vector<std::string> librariesToSkip = {"dyninst/install","cudadedup-develop", "/usr/lib64/librt-", "spectrum-mpi"};
+	static std::vector<std::string> librariesToSkip = {"dyninst/install","cudadedup-develop", "/usr/lib64/librt-", "spectrum-mpi", "ld-2.17", "libpthread-2.17.so"};
     std::string tmpLibname = func->getModule()->getObject()->pathName();
     for (auto i : librariesToSkip)
     	if (tmpLibname.find(i) != std::string::npos)

edcfc9494608273bed891177b361a55ec423d115
Ben Welton <welton@cs.wisc.edu>
2019-10-10 23:30:21 -0500

    Added

---
+++ src/driverapi/launcher/SerializeTypes.cpp
@@ -0,0 +1,71 @@
+#include "SerializeTypes.h"
+
+
+uint64_t SerializeStackRecMap(FILE * fp,std::map<uint64_t, StackRecord> & srmap) {
+	uint64_t ret = 0;
+	uint64_t size = srmap.size();
+	ret += SerializeUint64(fp, size);
+	for (auto i : srmap) {
+		ret += SerializeUint64(fp, i.first);
+		ret += i.second.SerializeStack(fp);
+	}
+	return ret;
+};
+
+void DeSerializeStackRecMap(FILE * fp, std::map<uint64_t, StackRecord> & srmap) {
+	uint64_t size = 0;
+	ReadUint64(fp, size);
+	for (int i = 0; i < size; i++) {
+		uint64_t tmp = 0;
+		StackRecord sr;
+		ReadUint64(fp, tmp);
+		sr.DeserializeStack(fp);
+		srmap[tmp] = sr;
+	}
+};
+
+uint64_t SerializeUint64(FILE * fp, uint64_t val) {
+	fwrite(&val, 1, sizeof(uint64_t), fp);
+	return sizeof(uint64_t);
+};
+
+void ReadUint64(FILE * fp, uint64_t & val) {
+	fread(&val, 1, sizeof(uint64_t), fp);
+	//return sizeof(uint64_t);
+};
+
+
+uint64_t SerializeSring(FILE * fp, std::string & str) {
+	uint64_t size = str.size();
+	fwrite(&size, 1, sizeof(uint64_t), fp);
+	if (size > 0)
+		fwrite(str.c_str(), sizeof(char), size, fp);
+	return sizeof(uint64_t) + size;
+};
+
+void DeserializeString(FILE * fp, std::string & ret) {
+	uint64_t size = 0;
+	ReadUint64(fp, size);
+	if (size > 0) {
+		std::shared_ptr<char> tmp(new char[size+1]);
+		fread(tmp.get(), sizeof(char), size, fp);
+		ret = std::string(tmp.get(), size);
+	}
+};
+
+uint64_t SerializeBool(FILE * fp, bool b) {
+	uint8_t bt = 0;
+	if(b)
+		bt = 1;
+	fwrite(&bt, 1, sizeof(uint8_t), fp);
+	return sizeof(uint8_t);
+};
+void DeserializeBool(FILE * fp, bool & b) {
+	uint8_t bt;
+	fread(&bt, 1, sizeof(uint8_t), fp);
+	if (bt == 1)
+		b = true;
+	else 
+		b = false;
+};
+
---
+++ src/driverapi/launcher/SerializeTypes.h
@@ -0,0 +1,56 @@
+#pragma once
+#include <string.h>
+#include <algorithm>
+#include <functional>
+#include <array>
+#include <iostream>
+#include <cassert>
+#include <deque>
+#include <sys/time.h>
+#include <cstdlib>
+#include <sstream>
+#include <tuple>
+#include <utility> 
+#include <stdarg.h>
+#include <map>
+#include <set> 
+#include <iomanip>
+#include <string>
+#include <cassert>
+#include <sys/types.h>
+#include <unistd.h>
+#include <mutex>
+#include <queue>
+#include <boost/program_options.hpp>
+
+// Dyninst includes
+#include "CodeObject.h"
+#include "CFG.h"
+#include "PatchObject.h"
+#include "PatchMgr.h"
+#include "Point.h"
+#include "BPatch_object.h"
+#include "BPatch_snippet.h"
+#include "BPatch.h"
+#include "BPatch_binaryEdit.h"
+#include "BPatch_image.h"
+#include "BPatch_function.h"
+#include "BPatch_Vector.h"
+#include "BPatch_point.h"
+#include "BPatch_addressSpace.h"
+#include "BPatch_statement.h"
+#include "dynC.h"
+#include "set"
+#include "LogInfo.h"
+#include "Constants.h"
+#include "StackPoint.h"
+#include "ReadStackKeys.h"
+
+uint64_t SerializeStackRecMap(FILE * fp,std::map<uint64_t, StackRecord> & srmap);
+void DeSerializeStackRecMap(FILE * fp, std::map<uint64_t, StackRecord> & srmap);
+uint64_t SerializeUint64(FILE * fp, uint64_t val);
+void ReadUint64(FILE * fp, uint64_t & val);
+uint64_t SerializeSring(FILE * fp, std::string & str);
+void DeserializeString(FILE * fp, std::string & ret);
+uint64_t SerializeBool(FILE * fp, bool b);
+void DeserializeBool(FILE * fp, bool & b);

8c49f91c44da0276e895686246f25c646fd64adc
Ben Welton <welton@cs.wisc.edu>
2019-10-18 17:56:06 -0500

    Added

--- src/driverapi/plugins/memrecorder/MemRecorder.cpp
+++ src/driverapi/plugins/memrecorder/MemRecorder.cpp
@@ -526,24 +526,24 @@ extern "C" {
 
 		DIOG_WriteTotals->mallocCount++;
 		//std::cerr << "We are here! (MALLOC)" << std::endl;
-		if (DIOGENES_GetWrapperStatus() || DIOGENES_TEAR_DOWN == true)
-			return;
+		//if (DIOGENES_GetWrapperStatus() || DIOGENES_TEAR_DOWN == true)
+		//	return;
 		//std::cerr << "We are here! (MALLOC)" << std::endl;
-		if (DIOGENES_CUDA_MALLOC_ARG == NULL)
-			return;
+		//if (DIOGENES_CUDA_MALLOC_ARG == NULL)
+		//	return;
 
-		if (DIOGENES_GetGlobalLock()) {
+		//if (DIOGENES_GetGlobalLock()) {
 			PLUG_BUILD_FACTORY();
 			// Fallback mode, do slow stack walk and save it to file.
 			std::vector<StackPoint> points;
 			bool ret = GET_FP_STACKWALK(points);
 			int64_t myID = static_cast<int64_t>(DIOGENES_MEM_KEYFILE->InsertStack(points));
-			PLUG_FACTORY_PTR->GPUMallocData((uint64_t)(*DIOGENES_CUDA_MALLOC_ARG), DIOGENES_CUDA_MALLOC_SIZE, myID);
+		//	PLUG_FACTORY_PTR->GPUMallocData((uint64_t)(*DIOGENES_CUDA_MALLOC_ARG), DIOGENES_CUDA_MALLOC_SIZE, myID);
 			std::cerr << "[DIOGENES::DIOG_CUDAMallocCheck] Unknown Malloc Entry for Identifier = " << myID << std::endl;
-			DIOGENES_CUDA_MALLOC_ARG = NULL;
-			DIOGENES_CUDA_MALLOC_SIZE = 0;
-			DIOGENES_ReleaseGlobalLock();
-		}
+		//	DIOGENES_CUDA_MALLOC_ARG = NULL;
+		//	DIOGENES_CUDA_MALLOC_SIZE = 0;
+		//	DIOGENES_ReleaseGlobalLock();
+		//}
 	} 
 	void DIOG_CUDAMallocCheck() {
 		if(DIOG_WriteTotals == NULL)

f94d3602f37c776f939ac742d5fda8b935309e9c
Ben Welton <welton@cs.wisc.edu>
2019-10-20 01:59:24 -0500

    Added

--- src/driverapi/launcher/StackPoint.h
+++ src/driverapi/launcher/StackPoint.h
@@ -330,12 +330,14 @@ struct StackKeyReader {
 		in = fp;
 	}
 	~StackKeyReader() {
-		fclose(in);
+		if (in  != NULL)
+			fclose(in);
 	}
 	std::map<uint64_t, std::vector<StackPoint> > ReadStacks() {
 		std::map<uint64_t, std::vector<StackPoint> > ret;
-		if (in == NULL)
+		if (in == NULL){
 			return ret;
+		}
 		fseek(in, 0, SEEK_END);
   		uint64_t size = ftell(in);
   		fseek(in, 0, SEEK_SET);

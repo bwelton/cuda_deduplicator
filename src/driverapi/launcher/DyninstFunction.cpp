578ba523f23adbc180e4c9e00c7d368340141d6f
Ben Welton <welton@cs.wisc.edu>
2019-10-29 02:15:15 -0500

    More changes

--- src/driverapi/launcher/DyninstFunction.cpp
+++ src/driverapi/launcher/DyninstFunction.cpp
@@ -359,6 +359,10 @@ uint64_t DyninstFunction::GetSmallestEntryBlockSize() {
 
 bool DyninstFunction::IsExcludedFunction(InstType T) {
 	//if(_track->ShouldInstrimentFunciton(_func, T))
+	if (!_track->ShouldInstrimentFunciton(_func, T))
+		std::cerr << "[DyninstFunction::IsExcludedFunction] Returned false for function" << std::endl;
+	if (!_track->ShouldInstrimentModule(_func, T))
+		std::cerr << "[DyninstFunction::IsExcludedFunction] Returned false for module" << std::endl;
 	if (_track->ShouldInstrimentFunciton(_func, T) && _track->ShouldInstrimentModule(_func, T))
 		return false;
 	return true;

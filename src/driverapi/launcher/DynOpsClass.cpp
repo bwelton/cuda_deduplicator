fb7f18c9b42745324ad42b0dc5d06714f75d0607
Benjamin Welton <welton@cs.wisc.edu>
2019-10-30 15:00:46 -0500

    added

--- src/driverapi/launcher/DynOpsClass.cpp
+++ src/driverapi/launcher/DynOpsClass.cpp
@@ -111,8 +111,8 @@ void DynOpsClass::GenerateAddrList(BPatch_addressSpace * aspace) {
 			auto start = n->getStartAddress();
 			auto end =  n->getEndAddress(); 
 			for (int z = start; z <= end; z++){
-				if (_addressList.find(z) != _addressList.end())
-					std::cerr << "Conflix between functions - " << _addressList[z]->getName() << " and " << i->getName() << " at addr " << z << std::endl;
+				//if (_addressList.find(z) != _addressList.end())
+				//	std::cerr << "Conflix between functions - " << _addressList[z]->getName() << " and " << i->getName() << " at addr " << z << std::endl;
 				_addressList[z] = i;
 			}
 		}

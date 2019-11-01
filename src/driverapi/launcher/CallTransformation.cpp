a34d09af3d2f0b6e22fe0da54f980aa5d196aaee
Ben Welton <welton@cs.wisc.edu>
2019-10-31 23:02:33 -0500

    More changes

--- src/driverapi/launcher/CallTransformation.cpp
+++ src/driverapi/launcher/CallTransformation.cpp
@@ -124,111 +124,115 @@ void CallTransformation::BuildRequiredSet() {
 	}	
 
 	for(auto i : couldNotFindStack) {
-		std::cerr << "[CallTransformation::BuildRequiredSet"
+		std::cerr << "[CallTransformation::BuildRequiredSet] Could not find a stack that matched with LS stack id " << i << std::endl;
 	}
 
-	LSStackGraphVec sgraph;
-	std::map<uint64_t, int> _mapToSgraph;
-	int pos = 0;
-	for (auto i : LS) {
-		sgraph.push_back(LSStackGraph(i.second, i.first));
-		_mapToSgraph[i.first] = pos;
-		if (matchSet.find(i.first) != matchSet.end())
-			sgraph.back()._idPointID = matchSet[i.first];
-		pos++;
+	for (auto i : notRequired) {
+		std::cerr << "[CallTransformation::BuildRequiredSet] Following synchronization is NOT required: " << typeMap[matchSet[i]] <<  " at LS stack = " << matchSet[i] << std::endl;
 	}
 
-	/*
-	StackPointTree tree(_idPoints);
-	for (auto & i : sgraph) {
-		if(i._found == true){
-			int64_t id = tree.FindID( i._beforeLibcuda);
-			i._idPointID = id;
-			if (id >= 0) {
-				std::cerr << "[CallTransformation::BuildRequiredSet] Found ID Match for Call - " << i._beforeLibcuda.libname << "@" << i._beforeLibcuda.libOffset << " [ID = " << id << "]" << std::endl;
-			} else {
-				std::cerr << "[CallTransformation::BuildRequiredSet] NO MATCH Match for Call - " << i._beforeLibcuda.libname << "@" << i._beforeLibcuda.libOffset << " [ID = " << id << "]" << std::endl;			
-			}
-		}
-	}*/
+// 	// LSStackGraphVec sgraph;
+// 	// std::map<uint64_t, int> _mapToSgraph;
+// 	// int pos = 0;
+// 	// for (auto i : LS) {
+// 	// 	sgraph.push_back(LSStackGraph(i.second, i.first));
+// 	// 	_mapToSgraph[i.first] = pos;
+// 	// 	if (matchSet.find(i.first) != matchSet.end())
+// 	// 		sgraph.back()._idPointID = matchSet[i.first];
+// 	// 	pos++;
+// 	// }
 
-	for (size_t i = 0; i < lvec.size(); i++) {
-		auto tmp = lvec[i];
-		if (tmp->newDependents) {
-			if (i < lvec.size() - 1) {
-				if (!traceDep.IsInSet(lvec[i+1]->id))
-					continue;
-			} 
-			auto it = _mapToSgraph.find(tmp->id);
-			if (it != _mapToSgraph.end()) {
-				sgraph[it->second]._required = true;
-			}
-		}
-	}
-/*
-	for (auto i : lvec) {
-		if(i->newDependents) {
-			auto it = _mapToSgraph.find(i->id);
-			if (it != _mapToSgraph.end()) {
-				sgraph[it->second]._required = true;
-				//std::cerr << "[CallTransformation::BuildRequiredSet] " 
-			}
-		}
-	}
-*/
-	std::vector<FreeSitePtr> freeSites;
+// 	/*
+// 	StackPointTree tree(_idPoints);
+// 	for (auto & i : sgraph) {
+// 		if(i._found == true){
+// 			int64_t id = tree.FindID( i._beforeLibcuda);
+// 			i._idPointID = id;
+// 			if (id >= 0) {
+// 				std::cerr << "[CallTransformation::BuildRequiredSet] Found ID Match for Call - " << i._beforeLibcuda.libname << "@" << i._beforeLibcuda.libOffset << " [ID = " << id << "]" << std::endl;
+// 			} else {
+// 				std::cerr << "[CallTransformation::BuildRequiredSet] NO MATCH Match for Call - " << i._beforeLibcuda.libname << "@" << i._beforeLibcuda.libOffset << " [ID = " << id << "]" << std::endl;			
+// 			}
+// 		}
+// 	}*/
 
-	for (auto i : sgraph) {
-		if (i._idPointID > 0) {
-			FreeSitePtr tmp = _gpuGraph.GetFreeSite(i._idPointID);
-			if (tmp != NULL) {
-				if (i._required == true) {
-					tmp->required = 1;
-					freeSites.push_back(tmp);
-				} else {
-					tmp->required = 0;
-					freeSites.push_back(tmp);
-				}
-			}
-		}
-	}
-	std::cerr << _gpuGraph.PrintRequiredSetFree() << std::endl;
-	std::vector<MallocPtr> mallocSites;	
-	_gpuGraph.TraverseFromPoint<FreeSitePtr,MallocPtr>(freeSites, mallocSites);
+// 	for (size_t i = 0; i < lvec.size(); i++) {
+// 		auto tmp = lvec[i];
+// 		if (tmp->newDependents) {
+// 			if (i < lvec.size() - 1) {
+// 				if (!traceDep.IsInSet(lvec[i+1]->id))
+// 					continue;
+// 			} 
+// 			auto it = _mapToSgraph.find(tmp->id);
+// 			if (it != _mapToSgraph.end()) {
+// 				sgraph[it->second]._required = true;
+// 			}
+// 		}
+// 	}
+// /*
+// 	for (auto i : lvec) {
+// 		if(i->newDependents) {
+// 			auto it = _mapToSgraph.find(i->id);
+// 			if (it != _mapToSgraph.end()) {
+// 				sgraph[it->second]._required = true;
+// 				//std::cerr << "[CallTransformation::BuildRequiredSet] " 
+// 			}
+// 		}
+// 	}
+// */
+// 	std::vector<FreeSitePtr> freeSites;
 
-	RemovePointsPtr ret(new RemovePoints());
-	for (auto i : freeSites) {
-		if (i->required != 0) {
-			ret->cudaFreeReqSync.push_back(i->p);
-			std::cout << "[REQ]" << i->Print() << std::endl;
-		} else {
-			ret->cudaFreeReplacements.push_back(i->p);
-			std::cout << "[REMOVE]" << i->Print() << std::endl;
-		}
-	}
+// 	for (auto i : sgraph) {
+// 		if (i._idPointID > 0) {
+// 			FreeSitePtr tmp = _gpuGraph.GetFreeSite(i._idPointID);
+// 			if (tmp != NULL) {
+// 				if (i._required == true) {
+// 					tmp->required = 1;
+// 					freeSites.push_back(tmp);
+// 				} else {
+// 					tmp->required = 0;
+// 					freeSites.push_back(tmp);
+// 				}
+// 			}
+// 		}
+// 	}
+// 	std::cerr << _gpuGraph.PrintRequiredSetFree() << std::endl;
+// 	std::vector<MallocPtr> mallocSites;	
+// 	_gpuGraph.TraverseFromPoint<FreeSitePtr,MallocPtr>(freeSites, mallocSites);
+
+// 	RemovePointsPtr ret(new RemovePoints());
+// 	for (auto i : freeSites) {
+// 		if (i->required != 0) {
+// 			ret->cudaFreeReqSync.push_back(i->p);
+// 			std::cout << "[REQ]" << i->Print() << std::endl;
+// 		} else {
+// 			ret->cudaFreeReplacements.push_back(i->p);
+// 			std::cout << "[REMOVE]" << i->Print() << std::endl;
+// 		}
+// 	}
 	
-	for (auto i : mallocSites) {
-		std::cout << "[WRAP]" << i->Print() << std::endl;
-		ret->cudaMallocReplacements.push_back(i->p);
-	}
+// 	for (auto i : mallocSites) {
+// 		std::cout << "[WRAP]" << i->Print() << std::endl;
+// 		ret->cudaMallocReplacements.push_back(i->p);
+// 	}
 
 
-	// cudaMemTransfer replacement
-	for (auto i : _transGraph.transfers) {
-		if (i.first == -1)
-			continue;
-		if(ret->CheckArrayAndAddToIndex(CUMEMCPY_REP, i.second->p) == NEW_ENTRY) {
-			ret->cudaMemcpyAsyncRepl.push_back(i.second->p);
-			MallocSiteSet tmp = i.second->GetMallocSites();
-			for (auto n : tmp) {
-				if(ret->CheckArrayAndAddToIndex(MALLOC_REP, n->p) == NEW_ENTRY && n->IsEmpty() == false) 
-					ret->mallocReplacements.push_back(n->p);
-			}
-		}
-	}
+// 	// cudaMemTransfer replacement
+// 	for (auto i : _transGraph.transfers) {
+// 		if (i.first == -1)
+// 			continue;
+// 		if(ret->CheckArrayAndAddToIndex(CUMEMCPY_REP, i.second->p) == NEW_ENTRY) {
+// 			ret->cudaMemcpyAsyncRepl.push_back(i.second->p);
+// 			MallocSiteSet tmp = i.second->GetMallocSites();
+// 			for (auto n : tmp) {
+// 				if(ret->CheckArrayAndAddToIndex(MALLOC_REP, n->p) == NEW_ENTRY && n->IsEmpty() == false) 
+// 					ret->mallocReplacements.push_back(n->p);
+// 			}
+// 		}
+// 	}
 
-	std::cout << ret->Print() << std::endl;
-	_removeCalls = ret;	
+// 	std::cout << ret->Print() << std::endl;
+// 	_removeCalls = ret;	
 }
 
 

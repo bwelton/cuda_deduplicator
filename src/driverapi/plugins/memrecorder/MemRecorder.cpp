4722e8cbe1b6707032bc8c1b6da2e4c1f1d597e8
Benjamin Welton <welton@cs.wisc.edu>
2019-11-13 14:37:19 -0600

    More changes

--- src/driverapi/plugins/memrecorder/MemRecorder.cpp
+++ src/driverapi/plugins/memrecorder/MemRecorder.cpp
@@ -538,22 +538,24 @@ extern "C" {
 	}
 
 	void POSTPROCESS_GNUFREE(uint64_t addr) {
-		if(DIOGENES_IN_MEMORYFREE == false && DIOGENES_GetGlobalLock() && DIOGENES_TEAR_DOWN == false) {
+		if (DIOGENES_IN_MEMORYFREE == false){
 			DIOGENES_IN_MEMORYFREE = true;
-			PLUG_BUILD_FACTORY();
-			//std::cout << "Processing Free at addr: " << std::hex << addr << std::endl;
-			std::shared_ptr<std::unordered_map<DIOG_IDNUMBER,StackPoint,EnumClassHash>> local = DIOG_GLOBAL_SPS;
-			std::vector<StackPoint> freeSiteStacks;
-			//DIOGENES_CACHED_POINTS.clear();
-			bool ret = GET_FP_STACKWALK(freeSiteStacks);
-			auto n = local->find(E_glibFree);
-			if (n == local->end())
-				assert(n != local->end());
-			freeSiteStacks.push_back(n->second);
-			int64_t myID = static_cast<int64_t>(DIOGENES_MEM_KEYFILE->InsertStack(freeSiteStacks));
-			PLUG_FACTORY_PTR->CPUFreeData(addr, myID);
-			DIOGENES_ReleaseGlobalLock();
-			DIOGENES_IN_MEMORYFREE =false;
+			if(DIOGENES_GetGlobalLock() && DIOGENES_TEAR_DOWN == false) {
+				PLUG_BUILD_FACTORY();
+				//std::cout << "Processing Free at addr: " << std::hex << addr << std::endl;
+				std::shared_ptr<std::unordered_map<DIOG_IDNUMBER,StackPoint,EnumClassHash>> local = DIOG_GLOBAL_SPS;
+				std::vector<StackPoint> freeSiteStacks;
+				//DIOGENES_CACHED_POINTS.clear();
+				bool ret = GET_FP_STACKWALK(freeSiteStacks);
+				auto n = local->find(E_glibFree);
+				if (n == local->end())
+					assert(n != local->end());
+				freeSiteStacks.push_back(n->second);
+				int64_t myID = static_cast<int64_t>(DIOGENES_MEM_KEYFILE->InsertStack(freeSiteStacks));
+				PLUG_FACTORY_PTR->CPUFreeData(addr, myID);
+				DIOGENES_ReleaseGlobalLock();
+			}
+			DIOGENES_IN_MEMORYFREE = false;
 		}
 	}
 

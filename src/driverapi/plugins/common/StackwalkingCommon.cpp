146015eda885fe230ce53df29fb94af507226368
Benjamin Welton <welton@cs.wisc.edu>
2019-11-13 13:12:18 -0600

    More changes

--- src/driverapi/plugins/common/StackwalkingCommon.cpp
+++ src/driverapi/plugins/common/StackwalkingCommon.cpp
@@ -10,6 +10,7 @@
 #include <cstring>
 #include <memory>
 #include <execinfo.h>
+#include <unordered_map>
 
 #include "StackPoint.h"
 #include "framestepper.h"
@@ -28,7 +29,63 @@ using namespace Dyninst::Stackwalker;
 using namespace SymtabAPI;
 
 thread_local Walker *  local_walker = NULL;
+// struct StackWalkerFastMap{
+// 	uint64_t _total_count;
+// 	std::map<uint64_t, StackWalkerFastMap *> _map;
+// 	StackWalkerFastMap() : _total_count(0) {};
+// 	void Init() {
+// 		// RAStackReaderWriter rs(fopen("AC_BinStacks.bin", "rb"));
+// 		// std::vector<std::vector<uint64_t>>  stacks = rs.ReadStacks();
+// 		// for (auto i : stacks) {
+// 		// 	Insert(i, 0);
+// 		// }
+// 		// _total_count = 0;
+// 		//StackKeyReader r(fopen("AC_AutoCorrectStacks.txt", "rb"));
+// 		//std::map<uint64_t, std::vector<StackPoint> > stacks = r.ReadStacks();
 
+// 	};
+// 	~StackWalkerFastMap() {
+// 		//DIOGENES_SHUTDOWN_MODE = true;
+// 		//if (_total_count != 0)
+// 		//	std::cout << "NUMBER OF OPTI HITS = " << std::dec << _total_count << std::endl;
+// 	};
+// 	void Insert(std::vector<uint64_t> & input, int pos) {
+// 		if(input.size() <= pos)
+// 			return;
+// 		uint64_t val = input[pos];
+// 		if (_map.find(val) == _map.end()) {
+// 			_map[val] = new StackWalkerFastMap();
+// 		}
+// 		_map[val]->Insert(input, pos+1);
+// 	};
+
+// 	bool Lookup(std::vector<uint64_t> & input, int pos) {
+// 		if(input.size() <= pos)
+// 			return true;
+// 		uint64_t val = input[pos];
+// 		if(_map.find(val) == _map.end())
+// 			return false;
+// 		return _map[val]->Lookup(input,pos+1);
+// 	};
+
+// 	bool IterativeLookup(uint64_t * input, int size, int pos) {
+// 		std::map<uint64_t, StackWalkerFastMap *> * curMap = &_map;
+// 		int insize = size;
+// 		while (pos < insize) {
+// 			auto it = curMap->find(input[pos]);
+// 			if (it == curMap->end()) 
+// 				return false;
+// 			curMap = &(it->second->_map);
+// 			pos++;
+// 		}
+// 		_total_count++;
+// 		return true;
+// 	};
+// };
+
+
+
+//thread_local StackWalkerFastMap fm;
 extern "C" {
 	void INIT_FP_STACKWALKER() {
 		if (local_walker != NULL)
@@ -48,6 +105,8 @@ extern "C" {
 		for (auto frame : stackwalk) {
 			StackPoint sp;
 			uint64_t offset;
+			std::cerr << "Frame RA/SP/FP: " << frame.getRA() << "," << frame.getSP() << "," << frame.getFP() << std::endl;
+
 			frame.getLibOffset(lib, offset, stab);
 			sp.libname = lib;
 			sp.libOffset = offset;

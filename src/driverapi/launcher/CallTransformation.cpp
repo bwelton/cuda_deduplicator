e7f7eb2028ed2725aa490d1bd471b2fe025caf9d
Ben Welton <welton@cs.wisc.edu>
2019-12-10 18:54:26 -0600

    More changes

--- src/driverapi/launcher/CallTransformation.cpp
+++ src/driverapi/launcher/CallTransformation.cpp
@@ -86,7 +86,7 @@ void CallTransformation::PrintStackSet(std::stringstream & outdata, std::string 
 
 }
 void CallTransformation::BuildRequiredSet() {
-	int printStackSize = 10;
+	int printStackSize = 20;
 	StackKeyReader r(fopen("LS_stackkey.txt","rb"));
 	std::map<uint64_t, std::string> typeMap;
 	std::map<uint64_t, std::vector<StackPoint> > LS = r.ReadStacks();
